571ae3aa1d2865b7c05f2338a48b81187a90e848
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2007-06-27 09:14:40 +0000

    * Upgraded to rails 1.2
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@2458 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- db/schema.rb
+++ db/schema.rb
@@ -5,33 +5,33 @@
 ActiveRecord::Schema.define(:version => 2) do
 
   create_table "information_pages", :force => true do |t|
-    t.column "parent_id", :integer
-    t.column "title", :string, :limit => 32, :default => "", :null => false
-    t.column "body", :text
+    t.column "parent_id",  :integer
+    t.column "title",      :string,   :limit => 32, :null => false
+    t.column "body",       :text
     t.column "created_at", :datetime
     t.column "updated_at", :datetime
   end
 
   create_table "news_items", :force => true do |t|
-    t.column "title", :string, :limit => 32, :default => "", :null => false
-    t.column "body", :text
+    t.column "title",      :string,   :limit => 32, :null => false
+    t.column "body",       :text
     t.column "created_at", :datetime
     t.column "updated_at", :datetime
   end
 
   create_table "users", :force => true do |t|
-    t.column "login", :string, :limit => 80, :default => "", :null => false
-    t.column "salted_password", :string, :limit => 40, :default => "", :null => false
-    t.column "email", :string, :limit => 60, :default => "", :null => false
-    t.column "first_name", :string, :limit => 40
-    t.column "last_name", :string, :limit => 40
-    t.column "salt", :string, :limit => 40, :default => "", :null => false
-    t.column "verified", :integer, :default => 0
-    t.column "role", :string, :limit => 40
-    t.column "security_token", :string, :limit => 40
-    t.column "token_expiry", :datetime
-    t.column "deleted", :integer, :default => 0
-    t.column "delete_after", :datetime
+    t.column "login",           :string,   :limit => 80,                :null => false
+    t.column "salted_password", :string,   :limit => 40,                :null => false
+    t.column "email",           :string,   :limit => 60,                :null => false
+    t.column "first_name",      :string,   :limit => 40
+    t.column "last_name",       :string,   :limit => 40
+    t.column "salt",            :string,   :limit => 40,                :null => false
+    t.column "verified",        :integer,                :default => 0
+    t.column "role",            :string,   :limit => 40
+    t.column "security_token",  :string,   :limit => 40
+    t.column "token_expiry",    :datetime
+    t.column "deleted",         :integer,                :default => 0
+    t.column "delete_after",    :datetime
   end
 
 end
