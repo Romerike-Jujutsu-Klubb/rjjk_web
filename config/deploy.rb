f1ff95e1febfec142dc7af5fd15cfcb0d99dde47
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2007-07-05 20:44:02 +0000

    * fikset deployment script
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@2486 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,19 @@
-set :application, "rjjk"
-set :repository, "svn+ssh://donv@source.kubosch.no/var/svn/trunk/rjjk_web"
+set :application, "rjjk_web"
+set :repository, "svn+ssh://donv@source.kubosch.no/var/svn/trunk/#{application}"
 
 role :app, "www.kubosch.no"
 role :db,  "www.kubosch.no", :primary => true
 
 set :user, "donv"
-
-ssh_options[:keys] = %w(/home/uwe/.ssh/id_rsa)
+set :use_sudo, false
 
 desc "The spinner task is used by :cold_deploy to start the application up"
 task :spinner, :roles => :app do
-  send(run_method, "/sbin/service rjjk start")
+  send(run_method, "/sbin/service #{application} start")
 end
 
 desc "Restart the mongrel server"
 task :restart, :roles => :app do
-  send(run_method, "/sbin/service rjjk restart")
+  send(run_method, "/sbin/service #{application} restart")
 end
 
