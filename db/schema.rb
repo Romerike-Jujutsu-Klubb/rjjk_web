9bf53d288db804566598c2daca6c18c410657069
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2007-08-27 20:13:04 +0000

    * Added the rest of the fields from the memeber import from CMS
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@2559 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 4) do
+ActiveRecord::Schema.define(:version => 5) do
 
   create_table "information_pages", :force => true do |t|
     t.column "parent_id",  :integer
@@ -13,32 +13,36 @@ ActiveRecord::Schema.define(:version => 4) do
   end
 
   create_table "members", :force => true do |t|
-    t.column "first_name",          :string,  :limit => 100, :default => "",    :null => false
-    t.column "last_name",           :string,  :limit => 100, :default => "",    :null => false
-    t.column "senior",              :boolean,                :default => false, :null => false
-    t.column "email",               :string,  :limit => 128
-    t.column "phone_mobile",        :string,  :limit => 32
-    t.column "phone_home",          :string,  :limit => 32
-    t.column "phone_work",          :string,  :limit => 32
-    t.column "phone_parent",        :string,  :limit => 32
-    t.column "birtdate",            :date
-    t.column "male",                :boolean,                                   :null => false
-    t.column "joined_on",           :date
-    t.column "contract_id",         :integer
-    t.column "department",          :string,  :limit => 100
-    t.column "cms_contract_id",     :integer
-    t.column "left_on",             :date
-    t.column "parent_name",         :string,  :limit => 100
-    t.column "address",             :string,  :limit => 100, :default => "",    :null => false
-    t.column "postal_code",         :string,  :limit => 4,                      :null => false
-    t.column "billing_type",        :string,  :limit => 100
-    t.column "billing_name",        :string,  :limit => 100
-    t.column "billing_address",     :string,  :limit => 100
-    t.column "billing_postal_code", :string,  :limit => 4
-    t.column "payment_problem",     :boolean,                                   :null => false
-    t.column "comment",             :string
-    t.column "instructor",          :boolean,                                   :null => false
-    t.column "nkf_fee",             :boolean,                                   :null => false
+    t.column "first_name",           :string,  :limit => 100, :default => "",    :null => false
+    t.column "last_name",            :string,  :limit => 100, :default => "",    :null => false
+    t.column "senior",               :boolean,                :default => false, :null => false
+    t.column "email",                :string,  :limit => 128
+    t.column "phone_mobile",         :string,  :limit => 32
+    t.column "phone_home",           :string,  :limit => 32
+    t.column "phone_work",           :string,  :limit => 32
+    t.column "phone_parent",         :string,  :limit => 32
+    t.column "birtdate",             :date
+    t.column "male",                 :boolean,                                   :null => false
+    t.column "joined_on",            :date
+    t.column "contract_id",          :integer
+    t.column "department",           :string,  :limit => 100
+    t.column "cms_contract_id",      :integer
+    t.column "left_on",              :date
+    t.column "parent_name",          :string,  :limit => 100
+    t.column "address",              :string,  :limit => 100, :default => "",    :null => false
+    t.column "postal_code",          :string,  :limit => 4,                      :null => false
+    t.column "billing_type",         :string,  :limit => 100
+    t.column "billing_name",         :string,  :limit => 100
+    t.column "billing_address",      :string,  :limit => 100
+    t.column "billing_postal_code",  :string,  :limit => 4
+    t.column "payment_problem",      :boolean,                                   :null => false
+    t.column "comment",              :string
+    t.column "instructor",           :boolean,                                   :null => false
+    t.column "nkf_fee",              :boolean,                                   :null => false
+    t.column "social_sec_no",        :string,  :limit => 6
+    t.column "account_no",           :string,  :limit => 16
+    t.column "billing_phone_home",   :string,  :limit => 32
+    t.column "billing_phone_mobile", :string,  :limit => 32
   end
 
   create_table "news_items", :force => true do |t|
