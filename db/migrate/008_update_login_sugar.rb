f62cf01992ddc49fc50bbbbcbf08715cd541c9aa
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2007-10-15 19:50:46 +0000

    * Changed for old PostgreSQL
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@2602 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- db/migrate/008_update_login_sugar.rb
+++ db/migrate/008_update_login_sugar.rb
@@ -1,12 +1,22 @@
 class UpdateLoginSugar < ActiveRecord::Migration
   def self.up
-      change_column :users, :verified, :boolean, :default => false
-      change_column :users, :deleted, :boolean, :default => false
+      add_column :users, :verified_tmp, :boolean, :default => false
+      execute('UPDATE users SET verified_tmp = true WHERE verified = 1')
+      execute('UPDATE users SET verified_tmp = false WHERE verified = 0')
+      remove_column :users, :verified
+      rename_column :users, :verified_tmp, :verified
+
+      add_column :users, :deleted_tmp, :boolean, :default => false
+      execute('UPDATE users SET deleted_tmp = true WHERE deleted = 1')
+      execute('UPDATE users SET deleted_tmp = false WHERE deleted = 0')
+      remove_column :users, :deleted
+      rename_column :users, :deleted_tmp, :deleted
+
       remove_column :users, :delete_after
   end
 
   def self.down
-      add_column :delete_after, :datetime, :default => nil
+      add_column :users, :delete_after, :datetime, :default => nil
       change_column :users, :deleted, :integer, :default => 0
       change_column :users, :verified, :integer, :default => 0
   end
