cef7d8197c34d78fcc1d5365aee09c42886f8bc9
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2008-01-02 15:27:28 +0000

    * Added display of SVN REVISION
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@2698 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- db/schema.rb
+++ db/schema.rb
@@ -102,8 +102,4 @@ ActiveRecord::Schema.define(:version => 9) do
     t.column "deleted",         :boolean,                :default => false
   end
 
-  create_table "weights", :force => true do |t|
-    t.column "weight", :decimal, :precision => 4, :scale => 1
-  end
-
 end
