7f5527938b0c5686e3ee9ef7d70b6bd17a95ca25
ondemannen <ondemannen@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2008-02-17 22:30:38 +0000

    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@2761 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/controllers/graduates_controller.rb
+++ app/controllers/graduates_controller.rb
@@ -1,36 +1,36 @@
 class GraduatesController < ApplicationController
-  MEMBERS_PER_PAGE = 30
+	MEMBERS_PER_PAGE = 30
 
-  before_filter :admin_required
+	before_filter :admin_required
 
-  def index
-    list
-    render :action => 'list'
-  end
+	def index
+		list
+		render :action => 'list'
+	end
 
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify :method => :post, :only => [ :destroy, :create, :update ],
          :redirect_to => { :action => :list }
 
-  def show_last_grade
-    @members = Member.find(:all)
-    @all_grades = @members.map {|m| m.current_grade}
-    @grade_counts = {}
-    @all_grades.each {|g|
-      @grade_counts[g] ||= 0
-      @grade_counts[g] += 1
-    }
-  end
+	def show_last_grade
+		@members = Member.find(:all)
+		@all_grades = @members.map {|m| m.current_grade}
+		@grade_counts = {}
+		@all_grades.each do |g|
+			@grade_counts[g] ||= 0
+			@grade_counts[g] += 1
+		end
+	end
   
-  def list
-    if !params[:id]
-      @graduate_pages = paginate :graduates, :conditions => "member_id > 0", :order => 'member_id, rank_id DESC'
-      @graduates = Graduate.find(:all, :conditions => "member_id > 0", :order => 'member_id, rank_id DESC')
-    else
-      @graduate_pages = paginate :graduates, :conditions => "member_id = #{params[:id]}", :order => 'rank_id'
-      @graduates = Graduate.find(:all, :conditions => "member_id = #{params[:id]}", :order => 'rank_id')
-    end
-  end
+	def list
+		if !params[:id]
+			@graduate_pages = paginate :graduates, :conditions => "member_id > 0", :order => 'member_id, rank_id DESC'
+			@graduates = Graduate.find(:all, :conditions => "member_id > 0", :order => 'member_id, rank_id DESC')
+		else
+			@graduate_pages = paginate :graduates, :conditions => "member_id = #{params[:id]}", :order => 'rank_id'
+			@graduates = Graduate.find(:all, :conditions => "member_id = #{params[:id]}", :order => 'rank_id')
+		end
+	end
 
   def list_graduates
     if params[:id]
@@ -77,10 +77,13 @@ class GraduatesController < ApplicationController
   </tr>
 EOC
     for cen in @censors
+		fn = cen.first_name.split(/\s+/).each { |x| x.capitalize!}.join(' ')
+		ln = cen.last_name.split(/\s+/).each { |x| x.capitalize!}.join(' ') 
+
       mbr = Member.find(:first, :conditions => ["cms_contract_id = ?", cen.member_id])
-      nm = "<td width=23%>#{mbr.first_name} #{mbr.last_name}</td>" <<
+      nm = "<td width=23%>#{fn} #{ln}</td>" <<
            "<td width=2%><a href=# onClick='removeCensor(#{cen.id}, " <<
-           "\"#{mbr.first_name} #{mbr.last_name}\");'>" <<
+           "\"#{fn} #{ln}\");'>" <<
            "<IMG SRC=\"/images/button-delete-16x16.png\" STYLE=\"border: 0;\" ALT=\"Slett sensor\"></a></td>\n"
       if ccnt == 0
         rstr << "<tr>" << nm
@@ -120,21 +123,23 @@ EOC
 EOH
     for gr in @graduates
       mbr = Member.find(:first, :conditions => [ "cms_contract_id = ?", gr.member_id])
+      fn = mbr.first_name.split(/\s+/).each { |x| x.capitalize!}.join(' ')
+      ln = mbr.last_name.split(/\s+/).each { |x| x.capitalize!}.join(' ') 
       rstr = rstr << "<tr id='#{gr.member_id}_#{gr.graduation.id}_view'>\n" <<
-                     "  <td>#{mbr.first_name} #{mbr.last_name}</td>\n" <<
+                     "  <td>#{fn} #{ln}</td>\n" <<
                      "  <td STYLE=\"text-align: center;\">#{gr.rank.name}</td>\n" <<
                      "  <td STYLE=\"text-align: center;\">#{gr.passed ? 'Ja' : 'Nei'}</td>\n" <<
                      "  <td STYLE=\"text-align: center;\">#{gr.paid_graduation ? 'Ja' : 'Nei'}</td>\n" <<
                      "  <td STYLE=\"text-align: center;\">#{gr.paid_belt ? 'Ja' : 'Nei'}</td>\n" <<
-                     "  <td STYLE=\"text-align: center;\"><A HREF='/graduates/list/" << gr.member_id.to_s << "'>" <<
+                     "  <td STYLE=\"text-align: center;\"><A HREF=\"/graduates/list/\"" << gr.member_id.to_s << "'>" <<
                      "<IMG SRC=\"/images/button-view-16x16.png\" STYLE=\"border: 0;\" ALT=\"Oversikt\"></A></td>\n" <<
                      "  <td STYLE=\"text-align: center;\"><A HREF='#' onClick='saveGraduateInfo(#{gr.member_id},#{gr.graduation.id})'>" <<
                      "<IMG SRC=\"/images/button-edit-16x16.png\" STYLE=\"border: 0;\" ALT=\"Endre\"></A></td>\n" <<
-                     "  <td STYLE=\"text-align: center;\"><A HREF='#' onClick='removeGraduate(#{gr.id},\"#{mbr.first_name} #{mbr.last_name}\")'>" <<
+                     "  <td STYLE=\"text-align: center;\"><A HREF='#' onClick='removeGraduate(#{gr.id},\"#{fn} #{ln}\")'>" <<
                      "<IMG SRC=\"/images/button-delete-16x16.png\" STYLE=\"border: 0;\" ALT=\"Slett\"></A></td>\n" <<
                      "</tr>\n" 
       rstr = rstr << "<tr id='#{gr.member_id}_#{gr.graduation.id}_edit' STYLE='display: none;'>\n" <<
-                     "  <td>#{mbr.first_name} #{mbr.last_name}</td>\n" <<
+                     "  <td>#{fn} #{ln}</td>\n" <<
                      #"  <td STYLE=\"text-align: center;\">#{gr.graduation.held_on}</td>\n" <<
                      "  <td STYLE=\"text-align: center;\">#{rank_select(gr.graduation.martial_art_id,gr.member_id,gr.rank.id)}</td>\n" <<
                      "  <td STYLE=\"text-align: center;\">#{yes_no_select('passed', gr.passed, gr.member_id)}</td>\n" <<
@@ -150,93 +155,95 @@ EOH
     render_text rstr << "</table>\n"
   end
 
-  def list_potential_graduates
-    @grads = Member.find(:all, :conditions => "left_on IS NULL", :order => 'last_name, first_name')
-    rstr =<<EOH
-    <div style="height:256px; width:284px; overflow:auto; overflow-x:hidden;">
-    <table STYLE="height: 128px;" width="100%" CELLPADDING="0" CELLSPACING="0">
-      <tr>
-        <td STYLE="background: #e3e3e3;border-top: 1px solid #000000;border-bottom: 1px solid #000000;"><B>Medlemmer</B></td>
-        <td STYLE="background: #e3e3e3;border-top: 1px solid #000000;border-bottom: 1px solid #000000;" ALIGN="right"><A HREF="#" onClick="hide_glist();">X</A></td>
-        <td width=20>&nbsp;</td>
-      </td><tr>
+	def list_potential_graduates
+		@grads = Member.find(:all, :conditions => "left_on IS NULL", :order => 'last_name, first_name',
+		                     :select => 'last_name, first_name, cms_contract_id, department')
+		rstr =<<EOH
+<div style="height:256px; width:256px; overflow:auto; overflow-x:hidden;">
+	<table WIDTH="256" CELLPADDING="0" CELLSPACING="0">
+	<tr>
+		<td STYLE="background: #e3e3e3;border-top: 1px solid #000000;border-bottom: 1px solid #000000;"<B>Medlemmer</B></td>
+		<td STYLE="background: #e3e3e3;border-top: 1px solid #000000;border-bottom: 1px solid #000000;" ALIGN="right"><A HREF="#" onClick="hide_glist();">X</A></td>
+		<td width=20>&nbsp;</td>
+	</td><tr>
 EOH
-    for grad in @grads
-      ln = grad.last_name.split(/\s+/).each { |x| x.capitalize!}.join(' ') 
-      fn = grad.first_name.split(/\s+/).each { |x| x.capitalize!}.join(' ')
-      rstr << "<tr>" <<
-              "<td align=left><a href='#' onClick='add_graduate(" + grad.cms_contract_id.to_s + ");'>" <<
-              "#{ln}, #{fn}</a></td>" <<
-              "<td ALIGN='right'>" << grad.department <<
-              "<td>&nbsp;</td></tr>\n"
-    end
-    render_text rstr << "</table>\n</div>\n"
-  end
+		for grad in @grads
+			ln = grad.last_name.split(/\s+/).each { |x| x.capitalize!}.join(' ')
+			fn = grad.first_name.split(/\s+/).each { |x| x.capitalize!}.join(' ')
+			rstr << "<tr>" <<
+			        "<td align=left><a href='#' onClick='add_graduate(" + grad.cms_contract_id.to_s + ");'>" <<
+			        "#{ln}, #{fn}</a></td>" <<
+			        "<td ALIGN='right'>&nbsp;" <<
+			        #"<td ALIGN='right'>" << grad.department <<
+			        "<td>&nbsp;</td></tr>\n"
+		end
+		render_text rstr << "</table>\n</div>\n"
+	end
 
-  def update_graduate
-    member_id = params[:member_id].to_i
-    rank_id = params[:rank_id].to_i 
-    graduate_id = params[:grad_id].to_i
-    passed = params[:passed].to_i 
-    paid = params[:paid].to_i
-    paid_belt = params[:paid_belt].to_i
-    
-    begin
-      @graduate = Graduate.find(:first, :conditions => [ "graduation_id = ? AND member_id = ?", graduate_id, member_id])
-      @graduate.update_attributes!(:rank_id => rank_id, :passed => passed, :paid_graduation => paid,
-                                  :paid_belt => paid_belt )
-      render_text "Endret graderingsinfo for medlems #{member_id}"
-    rescue StandardError
-      render_text "Det oppstod en feil ved endring av medlem #{member_id}:<P>" + $! + "</P>"
-    end
-  end
+	def update_graduate
+		member_id = params[:member_id].to_i
+		rank_id = params[:rank_id].to_i 
+		graduate_id = params[:grad_id].to_i
+		passed = params[:passed].to_i 
+		paid = params[:paid].to_i
+		paid_belt = params[:paid_belt].to_i
+		
+		begin
+			@graduate = Graduate.find(:first, :conditions => [ "graduation_id = ? AND member_id = ?", graduate_id, member_id])
+			@graduate.update_attributes!(:rank_id => rank_id, :passed => passed, :paid_graduation => paid,
+			                            :paid_belt => paid_belt )
+			render_text "Endret graderingsinfo for medlem. <!-- #{member_id} -->"
+		rescue StandardError
+			render_text "Det oppstod en feil ved endring av medlem #{member_id}:<P>" + $! + "</P>"
+		end
+	end
   
-  def show
-    @graduate = Graduate.find(params[:id])
-  end
+	def show
+		@graduate = Graduate.find(params[:id])
+	end
+	
+	def new
+		@graduate = Graduate.new
+	end
+	
+	def add_new_graduate
+		gid = params[:graduation_id].to_i
+		mid = params[:member_id].to_i
+		aid = params[:martial_arts_id].to_i
+		
+		rank = Rank.find(:first, :conditions => [ "martial_art_id = ?", aid])
+		Graduate.create!(:graduation_id => gid, :member_id => mid, :passed => false,
+		                 :rank_id => rank.id, :paid_graduation => false, :paid_belt => false)
+		render_text " " 
+	end
+	
+	
+	def create
+		@graduate = Graduate.new(params[:graduate])
+		if @graduate.save
+			flash[:notice] = 'Graduate was successfully created.'
+			redirect_to :action => 'list'
+		else
+			render :action => 'new'
+		end
+	end
+	
+	def edit
+		@graduate = Graduate.find(params[:id])
+	end
+	
+	def update
+		@graduate = Graduate.find(params[:id])
+		if @graduate.update_attributes(params[:graduate])
+			flash[:notice] = 'Graduate was successfully updated.'
+			redirect_to :action => 'show', :id => @graduate
+		else
+			render :action => 'edit'
+		end
+	end
 
-  def new
-    @graduate = Graduate.new
-  end
-  
-  def add_new_graduate
-    gid = params[:graduation_id].to_i
-    mid = params[:member_id].to_i
-    aid = params[:martial_arts_id].to_i
-    
-    rank = Rank.find(:first, :conditions => [ "martial_art_id = ?", aid])
-    Graduate.create!(:graduation_id => gid, :member_id => mid, :passed => false,
-                     :rank_id => rank.id, :paid_graduation => false, :paid_belt => false)
-    render_text " " 
-  end
-
-
-  def create
-    @graduate = Graduate.new(params[:graduate])
-    if @graduate.save
-      flash[:notice] = 'Graduate was successfully created.'
-      redirect_to :action => 'list'
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @graduate = Graduate.find(params[:id])
-  end
-
-  def update
-    @graduate = Graduate.find(params[:id])
-    if @graduate.update_attributes(params[:graduate])
-      flash[:notice] = 'Graduate was successfully updated.'
-      redirect_to :action => 'show', :id => @graduate
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    Graduate.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
+	def destroy
+		Graduate.find(params[:id]).destroy
+		redirect_to :action => 'list'
+	end
 end
