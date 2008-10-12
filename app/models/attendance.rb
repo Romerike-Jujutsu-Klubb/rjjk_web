4c99fa0cb5e42d7d2ee776954ab1a1dc16f03da5
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2008-10-12 08:11:08 +0000

    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@2987 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -8,4 +8,8 @@ class Attendance < ActiveRecord::Base
     Date.commercial(year, week, group_schedule.weekday)
   end
   
+  def self.find_member_count_for_month(group, year, month)
+    find(:all, :conditions => ['group_schedule_id IN ? AND year = ?', group.group_schedules.map{|gs| gs.id}, year, month])
+  end
+  
 end
