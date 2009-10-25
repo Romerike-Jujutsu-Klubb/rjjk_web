38adf1c15dbb07cec1c4e9d6e75487ac859c8a0b
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2009-10-25 22:12:51 +0000

    * Added JRUby 1.4.0RC2
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3291 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,4 +19,4 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors if you bad email addresses should just be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-config.threadsafe!
+# config.threadsafe!
