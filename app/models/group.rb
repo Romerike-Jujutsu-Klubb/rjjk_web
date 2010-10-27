d39cca95e066b2f4f3c1a6f3866ffe55bab0eff1
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2010-10-27 06:00:28 +0000

    * Added "active" scope to members
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3435 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
   belongs_to :martial_art
-  has_and_belongs_to_many :members, :conditions => 'left_on IS NULL'
+  has_and_belongs_to_many :members
   has_many :group_schedules
   
   def full_name
