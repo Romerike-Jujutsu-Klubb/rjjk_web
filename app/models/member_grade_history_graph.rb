53fdb1b72a097a531cfa53c05e5744a3a6b01168
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2011-01-14 00:02:07 +0000

    * La til graf over grader
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3508 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/member_grade_history_graph.rb
+++ app/models/member_grade_history_graph.rb
@@ -1,6 +1,4 @@
 class MemberGradeHistoryGraph
-  JUNIOR_AGE_LIMIT = 15
-  ASPIRANT_AGE_LIMIT = 10
   ACTIVE_CLAUSE = '"NOT EXISTS (SELECT kontraktsbelop FROM nkf_members WHERE member_id = members.id AND kontraktsbelop <= 0) AND (joined_on IS NULL OR joined_on <= \'#{date.strftime(\'%Y-%m-%d\')}\') AND (left_on IS NULL OR left_on > \'#{date.strftime(\'%Y-%m-%d\')}\')"'
   
   def self.history_graph(size = 480)
@@ -12,9 +10,8 @@ class MemberGradeHistoryGraph
     
     g = Gruff::Line.new(size)
     g.theme_37signals
-    g.title = "Antall aktive medlemmer"
+    g.title = "Fordeling av grader"
     g.font = '/usr/share/fonts/bitstream-vera/Vera.ttf'
-    #g.legend_font_size = 14
     g.hide_dots = true
     g.colors = %w{yellow yellow orange orange green green yellow yellow orange orange green green blue blue brown brown yellow orange green blue brown black black black}
     
@@ -24,7 +21,6 @@ class MemberGradeHistoryGraph
     dates = []
     Date.today.step(first_date, -14) {|date| dates << date}
     dates.reverse!
-    others = dates.map {|date| Member.count(:conditions => "(#{eval ACTIVE_CLAUSE}) AND birthdate IS NULL")}
     MartialArt.find_by_name('Kei Wa Ryu').ranks.each do |rank|
       g.data(rank.name, totals(rank, dates))
     end
@@ -37,8 +33,6 @@ class MemberGradeHistoryGraph
     dates.each_with_index {|date, i| if date.month != current_month && [1,8].include?(date.month) then labels[i] = (date.year != current_year ? "#{date.strftime("%m")}\n    #{date.strftime("%Y")}" : "#{date.strftime("%m")}") ; current_year = date.year ; current_month = date.month end}
     g.labels = labels
       
-    # g.draw_vertical_legend
-      
     precision = 1
     g.maximum_value = (g.maximum_value.to_s[0..precision].to_i + 1) * (10**(Math::log10(g.maximum_value.to_i).to_i - precision)) if g.maximum_value > 0
     g.maximum_value = [100, g.maximum_value].max
@@ -50,56 +44,11 @@ class MemberGradeHistoryGraph
     dates.map do |date|
       active_members = Member.all(
         :conditions => eval(ACTIVE_CLAUSE),
-        :include => {:graduates => :rank}
+        :include => {:graduates => [{:graduation => [:held_on, {:martial_art => :name}]}, :rank]}
       )
+      logger.debug "Active members: #{active_members.size}"
       active_members.select{|m| m.graduates.select{|g| g.graduation.martial_art.name =='Kei Wa Ryu'}.sort_by{|g| g.graduation.held_on}.last.try(:rank) == rank}.size
     end
   end
 
-  def self.seniors_jj(dates)
-    dates.map {|date| Member.count(
-        :conditions => "(#{eval ACTIVE_CLAUSE}) AND birthdate IS NOT NULL AND birthdate < '#{self.senior_birthdate(date)}' AND (martial_arts.name IS NULL OR martial_arts.name <> 'Aikikai')",
-        :include => {:groups => :martial_art}
-    )}
-  end
-
-  def self.juniors_jj(dates)
-    dates.map {|date| Member.count(
-        :conditions => ["(#{eval ACTIVE_CLAUSE}) AND birthdate IS NOT NULL AND birthdate BETWEEN ? AND ? AND (martial_arts.name IS NULL OR martial_arts.name <> 'Aikikai')", self.senior_birthdate(date), self.junior_birthdate(date)],
-        :include => {:groups => :martial_art}
-    )}
-  end
-  
-  def self.seniors_ad(dates)
-    dates.map {|date| Member.count(
-        :conditions => "(#{eval ACTIVE_CLAUSE}) AND birthdate IS NOT NULL AND birthdate < '#{self.senior_birthdate(date)}' AND martial_arts.name = 'Aikikai'",
-        :include => {:groups => :martial_art}
-    )}
-  end
-  
-  def self.juniors_ad(dates)
-    dates.map {|date| Member.count(
-        :conditions => "(#{eval ACTIVE_CLAUSE}) AND birthdate IS NOT NULL AND birthdate >= '#{self.senior_birthdate(date)}' AND martial_arts.name = 'Aikikai'",
-        :include => {:groups => :martial_art}
-    )}
-  end
-  
-  def self.aspirants(dates)
-    dates.map {|date| Member.count(
-        :conditions => "(#{eval ACTIVE_CLAUSE}) AND birthdate IS NOT NULL AND birthdate >= '#{self.junior_birthdate(date)}'"
-    )}
-  end
-  
-  def self.was_senior?(date)
-    birthdate.nil? or ((date - birthdate) / 365) > JUNIOR_AGE_LIMIT
-  end
-    
-  def self.senior_birthdate(date)
-    date - JUNIOR_AGE_LIMIT.years
-  end
-    
-  def self.junior_birthdate(date)
-    date - ASPIRANT_AGE_LIMIT.years
-  end
-    
 end
