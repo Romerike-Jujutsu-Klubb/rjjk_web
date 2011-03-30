db7239e70a3e4b3c60748670d96bb80a97c73dc4
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2011-03-30 17:30:43 +0000

    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3581 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -2,7 +2,7 @@ class StylesheetsController < ApplicationController
   layout false
 
   def dark_ritual
-    @content_width = (params[:width] && params[:width].to_i) || 620
+    @content_width = (params[:width] && params[:width].to_i) || 540
     render :content_type => 'text/css'
   end
   
