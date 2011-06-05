c2740e0d7ef8a5d9e0687507fac19de0f50e2b43
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2011-06-05 19:16:29 +0000

    * Tillate samme posisjon for grader i forskjellige stilarter
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3614 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -2,5 +2,5 @@ class Rank < ActiveRecord::Base
   belongs_to :martial_art
 
   validates_presence_of :position, :standard_months
-  validates_uniqueness_of :position
+  validates_uniqueness_of :position, :scope => :martial_art_id
 end
