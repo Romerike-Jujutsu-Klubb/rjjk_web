995b67a8d39bccae1e8ac2e5ad609d8900acd9f9
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2011-12-05 17:36:48 +0000

    * La til link til gruppe for hver grad.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3642 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

---
+++ db/migrate/20111205171636_add_group_id_to_ranks.rb
@@ -0,0 +1,15 @@
+class AddGroupIdToRanks < ActiveRecord::Migration
+  def self.up
+    add_column :ranks, :group_id, :integer
+    Rank.reset_column_information
+    g = Group.find_by_name('Tiger')
+    Rank.all.each do |r|
+      r.update_attributes! :group_id => g.id
+    end
+    change_column :ranks, :group_id, :integer, :null => false
+  end
+
+  def self.down
+    remove_column :ranks, :group_id
+  end
+end
