b5aff21f309c5c8aef809404c08c89f99ae8e00d
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2011-12-05 17:47:06 +0000

    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3645 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,6 +1,7 @@
 class Rank < ActiveRecord::Base
   belongs_to :martial_art
+  belongs_to :group
 
-  validates_presence_of :position, :standard_months
+  validates_presence_of :position, :standard_months, :group, :group_id, :martial_art, :martial_art_id
   validates_uniqueness_of :position, :scope => :martial_art_id
 end
