cc7f83bbd39342d1ea845e3cae7331f09b8c7e59
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2011-12-05 20:34:22 +0000

    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3664 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/controllers/graduates_controller.rb
+++ app/controllers/graduates_controller.rb
@@ -226,9 +226,13 @@ EOH
     if member.current_rank
       next_rank = Rank.find(:first, :conditions => ['martial_art_id = ? AND position = ?', ma, member.current_rank.position + 1])
     else
+      p member.current_rank
+      p member
+      p ma.ranks
+      p ma.ranks.select{|r| member.age >= r.minimum_age && (r.group.from_age..r.group.to_age).include?(member.age)}
+      p ma.ranks.select{|r| member.age >= r.minimum_age && (r.group.from_age..r.group.to_age).include?(member.age)}.first
       next_rank = ma.ranks.select{|r| member.age >= r.minimum_age && (r.group.from_age..r.group.to_age).include?(member.age)}.first
     end
-    # rank = Rank.find(:first, :conditions => [ "martial_art_id = ?", aid])
     raise "Unable to find rank for martial art with id = #{aid}" unless next_rank
 
     Graduate.create!(:graduation_id => gid, :member_id => mid, :passed => true,
