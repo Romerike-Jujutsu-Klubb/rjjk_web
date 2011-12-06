1ff919ea6a04fe9b33c5edb8d8ff480eddde941c
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2011-12-06 12:47:12 +0000

    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3691 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,16 +3,16 @@ class Member < ActiveRecord::Base
   ASPIRANT_AGE_LIMIT = 10
   MEMBERS_PER_PAGE = 30
   ACTIVE_CONDITIONS = "left_on IS NULL or left_on > DATE(CURRENT_TIMESTAMP)"
-  
-  named_scope :active, lambda { |date| { :conditions => ['left_on IS NULL OR left_on > ?', date] }}
+
+  named_scope :active, lambda { |date| {:conditions => ['left_on IS NULL OR left_on > ?', date]} }
 
   has_many :graduates
   has_many :attendances
   has_and_belongs_to_many :groups
   has_one :nkf_member
-  
+
   # validates_presence_of :address, :cms_contract_id
-  validates_length_of :billing_postal_code, :is => 4, :if => Proc.new{|m|m.billing_postal_code && !m.billing_postal_code.empty?}
+  validates_length_of :billing_postal_code, :is => 4, :if => Proc.new { |m| m.billing_postal_code && !m.billing_postal_code.empty? }
   # validates_presence_of :birthdate, :join_on
   validates_uniqueness_of :cms_contract_id, :if => :cms_contract_id
   validates_presence_of :first_name, :last_name
@@ -21,42 +21,42 @@ class Member < ActiveRecord::Base
   validates_inclusion_of :nkf_fee, :in => [true, false]
   validates_inclusion_of :payment_problem, :in => [true, false]
   #validates_presence_of :postal_code
-  validates_length_of :postal_code, :is => 4, :if => Proc.new {|m|m.postal_code && !m.postal_code.empty?}
-  validates_uniqueness_of :rfid, :if => Proc.new{|r| r.rfid and not r.rfid.empty?}
-  
+  validates_length_of :postal_code, :is => 4, :if => Proc.new { |m| m.postal_code && !m.postal_code.empty? }
+  validates_uniqueness_of :rfid, :if => Proc.new { |r| r.rfid and not r.rfid.empty? }
+
   def self.find_active
     find(:all, :conditions => ACTIVE_CONDITIONS, :order => 'last_name, first_name')
   end
-  
+
   def self.paginate_active(page)
     paginate :page => page, :per_page => MEMBERS_PER_PAGE, :conditions => ACTIVE_CONDITIONS, :order => 'first_name, last_name'
   end
-  
+
   def self.count_active
-    count :conditions => ACTIVE_CONDITIONS 
+    count :conditions => ACTIVE_CONDITIONS
   end
-  
+
   def self.find_by_contents(query, options = {})
     search_fields = [:first_name, :last_name, :email]
     all({
-      :conditions => [search_fields.map{|c| "UPPER(#{c}) LIKE ?"}.join(' OR '), *(["%#{query.upcase}%"] * search_fields.size)],
-      :order => 'first_name, last_name',
-    }.update(options))
+            :conditions => [search_fields.map { |c| "UPPER(#{c}) LIKE ?" }.join(' OR '), *(["%#{query.upcase}%"] * search_fields.size)],
+            :order => 'first_name, last_name',
+        }.update(options))
   end
-  
+
   def current_graduate(martial_art, date = Date.today)
-    graduates.select{|g| g.graduation.held_on < date && (martial_art.nil? || g.rank.martial_art == martial_art)}.sort_by {|g| g.rank.position}.last
+    graduates.select { |g| g.graduation.held_on < date && (martial_art.nil? || g.rank.martial_art == martial_art) }.sort_by { |g| g.rank.position }.last
   end
-  
+
   def current_rank(martial_art = nil, date = Date.today)
     current_graduate(martial_art, date).try(:rank)
   end
-  
+
   def current_rank_date(martial_art = nil)
     graduate = self.current_graduate(martial_art)
     graduate && graduate.graduation.held_on || joined_on
   end
-  
+
   def current_rank_age(martial_art = nil)
     date = current_rank_date
     days = (Date.today - date).to_i
@@ -69,12 +69,13 @@ class Member < ActiveRecord::Base
     age = self.age(graduation.held_on)
     ma = graduation.martial_art
     current_rank = current_rank(ma, graduation.held_on)
-    future_ranks = graduates.select{|g| g.graduation.martial_art == ma && g.graduation.held_on >= graduation.held_on}.map(&:rank)
+    future_ranks = graduates.select { |g| g.graduation.martial_art == ma && g.graduation.held_on >= graduation.held_on }.map(&:rank)
     if current_rank
-      next_rank = ma.ranks.select{|r| !future_ranks.include?(r) && r.position > current_rank.position && age >= r.minimum_age && (r.group.from_age..r.group.to_age).include?(age)}.first
+      next_rank = ma.ranks.select { |r| !future_ranks.include?(r) && r.position > current_rank.position &&
+          age >= r.minimum_age && (r.group.from_age..r.group.to_age).include?(age) }.first
       next_rank ||= Rank.find(:first, :conditions => ['martial_art_id = ? AND position = ?', ma, current_rank.position + 1])
     end
-    next_rank ||= ma.ranks.select{|r| age >= r.minimum_age && (r.group.from_age..r.group.to_age).include?(age)}.first
+    next_rank ||= ma.ranks.select { |r| age >= r.minimum_age && (r.group.from_age..r.group.to_age).include?(age) }.first
     next_rank ||= Rank.first
     next_rank
   end
@@ -88,64 +89,64 @@ class Member < ActiveRecord::Base
       260 + nkf_fee_amount
     end
   end
-  
+
   def senior?
     birthdate && (age >= JUNIOR_AGE_LIMIT)
   end
-  
+
   def nkf_fee_amount
     if senior?
-     (nkf_fee? ? (279.0 / 12).ceil : 0)
+      (nkf_fee? ? (279.0 / 12).ceil : 0)
     else
-     (nkf_fee? ? (155.0 / 12).ceil : 0)  
+      (nkf_fee? ? (155.0 / 12).ceil : 0)
     end
   end
-  
+
   def age(date = Date.today)
     return nil unless birthdate
     age = date.year - birthdate.year
     age -= 1 if date < birthdate + age.years
     age
   end
-  
+
   def age_group
     return nil unless age
     if age >= JUNIOR_AGE_LIMIT
-        "Senior"
+      "Senior"
     else
-        "Junior"
+      "Junior"
     end
   end
-  
-  def gender    
+
+  def gender
     if male
-        "Mann"
+      "Mann"
     else
-        "Kvinne"
+      "Kvinne"
     end
   end
-  
+
   def name
-      "#{first_name} #{last_name}"
+    "#{first_name} #{last_name}"
   end
-  
+
   def image_file=(file)
     return if file == ""
     self.image = file.read
     self.image_name = file.original_filename
     self.image_content_type = file.content_type
   end
-  
+
   def image_format
     image_name && image_name.split('.').last
   end
-  
+
   def thumbnail(x = 120, y = 160)
     return unless self.image
     magick_image = Magick::Image.from_blob(self.image).first
     return magick_image.crop_resized(x, y).to_blob
   end
-  
+
   def cms_member
     CmsMember.find_by_cms_contract_id(cms_contract_id)
   end
