e0e84097adc35c6988e7a1c44478be1c6e9b646f
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2012-01-24 11:25:07 +0000

    * Oppdaterte til Rails 3.1
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3773 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ RjjkWeb::Application.routes.draw do
   match ':controller/create', :action => 'create'
   match ':controller/destroy', :action => 'destroy'
   match ':controller/excel_export', :action => 'excel_export'
-  match ':controller/history_graph', :action => 'history_graph'
   match ':controller/yaml', :action => 'yaml'
 
   resources :attendances
