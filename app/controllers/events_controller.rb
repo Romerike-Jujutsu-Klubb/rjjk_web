b43c82a71de729da7625e0a706a7d2ce1b510ffd
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2012-06-14 23:35:17 +0000

    * La til utsending av invitasjon til aktivitet.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@3995 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,25 +1,25 @@
 class EventsController < ApplicationController
   before_filter :admin_required, :except => [:index, :show]
-  
+
   def index
     @events = Event.all
   end
-  
+
   def show
     @event = Event.find(params[:id])
   end
-  
+
   def new
     @event = Event.new
   end
-  
+
   def edit
     @event = Event.find(params[:id])
   end
-  
+
   def create
     @event = Event.new(params[:event])
-    
+
     if @event.save
       flash[:notice] = 'Event was successfully created.'
       redirect_to(@event)
@@ -27,7 +27,7 @@ class EventsController < ApplicationController
       render :action => "new"
     end
   end
-  
+
   def update
     @event = Event.find(params[:id])
     if @event.update_attributes(params[:event])
@@ -37,7 +37,7 @@ class EventsController < ApplicationController
       render :action => "edit"
     end
   end
-  
+
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
@@ -47,11 +47,13 @@ class EventsController < ApplicationController
   def invite
     event = Event.find(params[:id])
     if params[:example]
-      recipient = current_user.email
+      recipients = [current_user.email]
     elsif params[:recipients] == 'all'
-      recipient = Group.all.map {|g| g.members.active(event.start_at.to_date).map {|m| m.email }}.flatten.compact.sort.uniq.join(',')
+      recipients = Group.all.map { |g| g.members.active(event.start_at.to_date).map { |m| m.email } }.flatten.compact.sort.uniq
     end
-    NewsletterMailer.event_invitation(event, recipient).deliver
+    recipients.each do |recipient|
+      NewsletterMailer.event_invitation(event, recipient).deliver
+    end
     render :text => ''
   end
 end
