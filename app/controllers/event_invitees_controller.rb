3e84722900aba4cea5bcb6da0a4fa60c807f1572
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2012-09-25 16:01:32 +0000

    * La til registrering av hvem som har sagt de kommer og hvem som har betalt.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4041 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

---
+++ app/controllers/event_invitees_controller.rb
@@ -0,0 +1,83 @@
+class EventInviteesController < ApplicationController
+  # GET /event_invitees
+  # GET /event_invitees.json
+  def index
+    @event_invitees = EventInvitee.order(:name).all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @event_invitees }
+    end
+  end
+
+  # GET /event_invitees/1
+  # GET /event_invitees/1.json
+  def show
+    @event_invitee = EventInvitee.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @event_invitee }
+    end
+  end
+
+  # GET /event_invitees/new
+  # GET /event_invitees/new.json
+  def new
+    @event_invitee = EventInvitee.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @event_invitee }
+    end
+  end
+
+  # GET /event_invitees/1/edit
+  def edit
+    @event_invitee = EventInvitee.find(params[:id])
+  end
+
+  # POST /event_invitees
+  # POST /event_invitees.json
+  def create
+    @event_invitee = EventInvitee.new(params[:event_invitee])
+
+    respond_to do |format|
+      if @event_invitee.save
+        format.html { redirect_to @event_invitee, notice: 'Event invitee was successfully created.' }
+        format.json { render json: @event_invitee, status: :created, location: @event_invitee }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @event_invitee.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /event_invitees/1
+  # PUT /event_invitees/1.json
+  def update
+    @event_invitee = EventInvitee.find(params[:id])
+
+    respond_to do |format|
+      if @event_invitee.update_attributes(params[:event_invitee])
+        format.html { back_or_redirect_to @event_invitee, notice: 'Event invitee was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @event_invitee.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /event_invitees/1
+  # DELETE /event_invitees/1.json
+  def destroy
+    @event_invitee = EventInvitee.find(params[:id])
+    @event_invitee.destroy
+
+    respond_to do |format|
+      format.html { redirect_to event_invitees_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ test/functional/event_invitees_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class EventInviteesControllerTest < ActionController::TestCase
+  setup do
+    @event_invitee = event_invitees(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:event_invitees)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create event_invitee" do
+    assert_difference('EventInvitee.count') do
+      post :create, event_invitee: { address: @event_invitee.address, email: @event_invitee.email, event_id: @event_invitee.event_id, name: @event_invitee.name, organization: @event_invitee.organization, payed: @event_invitee.payed, will_attend_boolean: @event_invitee.will_attend_boolean }
+    end
+
+    assert_redirected_to event_invitee_path(assigns(:event_invitee))
+  end
+
+  test "should show event_invitee" do
+    get :show, id: @event_invitee
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @event_invitee
+    assert_response :success
+  end
+
+  test "should update event_invitee" do
+    put :update, id: @event_invitee, event_invitee: { address: @event_invitee.address, email: @event_invitee.email, event_id: @event_invitee.event_id, name: @event_invitee.name, organization: @event_invitee.organization, payed: @event_invitee.payed, will_attend_boolean: @event_invitee.will_attend_boolean }
+    assert_redirected_to event_invitee_path(assigns(:event_invitee))
+  end
+
+  test "should destroy event_invitee" do
+    assert_difference('EventInvitee.count', -1) do
+      delete :destroy, id: @event_invitee
+    end
+
+    assert_redirected_to event_invitees_path
+  end
+end
