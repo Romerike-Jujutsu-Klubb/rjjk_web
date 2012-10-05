4f6ddd19dd5a0e5996cac095d6c439111930b610
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2012-10-05 14:33:08 +0000

    * Bedre liste for påmeldinger.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4069 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- copy_production_to_development.sh
+++ copy_production_to_development.sh
@@ -4,8 +4,8 @@ DB_NAME=rjjk_web_development
 DUMP_FILE=rjjk_production_`date +'%Y-%m-%d'`.sql.gz
 
 echo "Disconnecting clients"
-echo "SELECT procpid, (SELECT pg_terminate_backend(procpid)) as killed from pg_stat_activity
-    WHERE datname = '$DB_NAME' AND current_query LIKE '<IDLE>';" | psql -h localhost $DB_NAME
+echo "SELECT pid, (SELECT pg_terminate_backend(pid)) as killed from pg_stat_activity
+    WHERE datname = '$DB_NAME' AND state = 'idle';" | psql -h localhost $DB_NAME
 
 echo "Re-creating database"
 dropdb -h localhost $DB_NAME # --if-exists
