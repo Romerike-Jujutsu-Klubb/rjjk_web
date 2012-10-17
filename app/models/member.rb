71483fd9360c336e3ec3789928377b14c78f1690
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2012-10-17 17:59:34 +0000

    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4080 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/member.rb
+++ app/models/member.rb
@@ -48,7 +48,7 @@ class Member < ActiveRecord::Base
   end
 
   def self.find_by_contents(query, options = {})
-    search_fields = [:first_name, :last_name, :email]
+    search_fields = [:first_name, :last_name, :email, :phone_mobile, :phone_home, :phone_parent, :phone_work]
     all({
             :conditions => [search_fields.map { |c| "UPPER(#{c}) LIKE ?" }.join(' OR '), *(["%#{query.upcase}%"] * search_fields.size)],
             :order => 'first_name, last_name',
