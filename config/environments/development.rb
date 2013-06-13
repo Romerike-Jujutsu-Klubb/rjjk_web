ca2c20d867e1832f243d8154dc62a890871ddc22
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-06-13 09:27:39 +0000

    * Fjernet kommentarer fra config for lettere lesing.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4182 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,13 @@
 RjjkWeb::Application.configure do
   config.cache_classes = false
   config.whiny_nils = true
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :sendmail
   # config.action_mailer.sendmail_settings = {:arguments => '-i'}
-  config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000 }
+  config.action_mailer.default_url_options = {:host => 'localhost', :port => 3000}
   config.active_support.deprecation = :log
   config.action_dispatch.best_standards_support = :builtin
   config.serve_static_assets = true
