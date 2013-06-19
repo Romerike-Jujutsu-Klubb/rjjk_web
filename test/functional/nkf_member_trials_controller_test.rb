242fd27327da28e977d8f2e8a1de5be6f1dc6635
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-06-19 21:55:49 +0000

    * Krever nå admin-tilgang for å behandle "trials".
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4210 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- test/functional/nkf_member_trials_controller_test.rb
+++ test/functional/nkf_member_trials_controller_test.rb
@@ -1,18 +1,22 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class NkfMemberTrialsControllerTest < ActionController::TestCase
-  test "should get index" do
+  def setup
+    login(:admin)
+  end
+
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:nkf_member_trials)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create nkf_member_trial" do
+  test 'should create nkf_member_trial' do
     assert_difference('NkfMemberTrial.count') do
       post :create, :nkf_member_trial => {
           :adresse => 'vei', :alder => 16, :epost => 'werwer@ertrt.br', :epost_faktura => 'sefsdfd@sdfsdf.com',
@@ -26,23 +30,23 @@ class NkfMemberTrialsControllerTest < ActionController::TestCase
     assert_redirected_to nkf_member_trial_path(assigns(:nkf_member_trial))
   end
 
-  test "should show nkf_member_trial" do
+  test 'should show nkf_member_trial' do
     get :show, :id => nkf_member_trials(:one).to_param
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, :id => nkf_member_trials(:one).to_param
     assert_response :success
   end
 
-  test "should update nkf_member_trial" do
+  test 'should update nkf_member_trial' do
     put :update, :id => nkf_member_trials(:one).to_param, :nkf_member_trial => { }
     assert_no_errors :nkf_member_trial
     assert_redirected_to nkf_member_trial_path(assigns(:nkf_member_trial))
   end
 
-  test "should destroy nkf_member_trial" do
+  test 'should destroy nkf_member_trial' do
     assert_difference('NkfMemberTrial.count', -1) do
       delete :destroy, :id => nkf_member_trials(:one).to_param
     end
