7d57f41f78fda067b3c92410c071aa56846fdb13
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-07-03 11:53:27 +0000

    * Ordnung muss sein!
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4294 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/environments/beta.rb
+++ config/environments/beta.rb
@@ -1,16 +1,16 @@
 RjjkWeb::Application.configure do
+  config.action_controller.perform_caching = true
+  config.active_support.deprecation = :notify
   config.cache_classes = true
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-  config.serve_static_assets = false
-  config.assets.compress = true
   config.assets.compile = false
+  config.assets.compress = true
   config.assets.digest = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'beta.jujutsu.no' }
-  config.threadsafe!
+  config.action_mailer.raise_delivery_errors = true
   config.i18n.fallbacks = true
-  config.active_support.deprecation = :notify
+  config.serve_static_assets = false
+  config.threadsafe!
 end
 
 email_notification_options = {:email => {
