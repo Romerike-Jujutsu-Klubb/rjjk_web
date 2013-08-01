87ae2a3ed0dc36f91f9d12b17d2e34e6698ed3b6
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-08-01 12:24:20 +0000

    * Bruk e-post layout for instruksjonspåmindelser.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4358 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/mailers/instruction_mailer.rb
+++ app/mailers/instruction_mailer.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class InstructionMailer < ActionMailer::Base
+  layout 'email'
   default from: Rails.env == 'production' ? 'webmaster@jujutsu.no' : "#{Rails.env}@jujutsu.no",
           to: Rails.env == 'production' ? %w(uwe@kubosch.no) : 'uwe@kubosch.no'
 
