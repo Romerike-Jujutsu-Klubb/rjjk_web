06f9cfb204219a64c392bacebcf61880f707cbb1
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-08-01 12:30:17 +0000

    * Bruk e-post layout for innmeldingspåmindelser.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4359 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/mailers/nkf_member_trial_mailer.rb
+++ app/mailers/nkf_member_trial_mailer.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 class NkfMemberTrialMailer < ActionMailer::Base
+  layout 'email'
   default from: Rails.env == 'production' ? 'webmaster@jujutsu.no' : "#{Rails.env}@jujutsu.no",
-          to: Rails.env == 'production' ? %w(medlem@jujutsu.no uwe@kubosch.no) : 'uwe@kubosch.no'
+          to: Rails.env == 'production' ? %w(medlem@jujutsu.no uwe@kubosch.no) : '"medlem@jujutsu.no" <uwe@kubosch.no>'
 
   def notify_trial_end
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @greeting = 'Hi'
+    mail to: 'to@example.org'
   end
 
   def notify_overdue_trials(trials)
