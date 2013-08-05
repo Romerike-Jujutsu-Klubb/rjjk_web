951d6cf7e882d311c32d1375f0bf44548f946933
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-08-05 21:19:11 +0000

    * Prøver å få raskere utplassering til beta.jujutsu.no
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4376 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/deploy/beta.rb
+++ config/deploy/beta.rb
@@ -2,6 +2,7 @@ set :application, 'rjjk_web_beta'
 set :scm, :none
 set :repository, '.'
 set :deploy_via, :copy
+set :copy_via, :scp
 set :copy_local_tar, '/usr/bin/gnutar' if `uname` =~ /Darwin/
 set :keep_releases, 1
 after 'deploy:update', 'deploy:cleanup'
