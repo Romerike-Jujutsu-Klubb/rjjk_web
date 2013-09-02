0bc6458868d259a8def7eb06152c59e44459b40e
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-09-02 17:26:18 +0000

    * Slapp av litt med å finnne instruktører.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4405 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/environment.rb
+++ config/environment.rb
@@ -10,25 +10,25 @@ require 'schema_plus_fix'
 RjjkWeb::Application.initialize!
 
 #FIXME(uwe): Report to RedCloth
-if RUBY_PLATFORM =~ /java/
-  require 'redcloth/textile_doc'
-  module RedCloth
-    class TextileDoc
-      def initialize(string, restrictions = [])
-        restrictions.each { |r| method("#{r}=").call(true) }
-        super(string.chars.map { |x| x.bytesize > 1 ? "&##{x.unpack('U*').first};" : x }.join)
-      end
-
-      def to_plain(*rules)
-        apply_rules(rules)
-        output = to(Formatters::Plain)
-        output.force_encoding(Encoding::UTF_8)
-        output = Formatters::Plain::Sanitizer.strip_tags(output)
-        output.gsub('!LINK_OPEN_TAG!', '<').gsub('!LINK_CLOSE_TAG!', '>')
-      end
-    end
-  end
-end
+#if RUBY_PLATFORM =~ /java/
+#  require 'redcloth/textile_doc'
+#  module RedCloth
+#    class TextileDoc
+#      def initialize(string, restrictions = [])
+#        restrictions.each { |r| method("#{r}=").call(true) }
+#        super(string.chars.map { |x| x.bytesize > 1 ? "&##{x.unpack('U*').first};" : x }.join)
+#      end
+#
+#      def to_plain(*rules)
+#        apply_rules(rules)
+#        output = to(Formatters::Plain)
+#        output.force_encoding(Encoding::UTF_8)
+#        output = Formatters::Plain::Sanitizer.strip_tags(output)
+#        output.gsub('!LINK_OPEN_TAG!', '<').gsub('!LINK_CLOSE_TAG!', '>')
+#      end
+#    end
+#  end
+#end
 
 ActionView::Base.field_error_proc = Proc.new { |html_tag, _| "<span class=\"fieldWithErrors\">#{html_tag}</span>".html_safe }
 
