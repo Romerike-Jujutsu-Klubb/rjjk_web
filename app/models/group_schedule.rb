2f350b1125fed9bb8e6f30a9f2796c1c315d6101
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-10-13 10:28:40 +0000

    * Gjenbruk dagnavn fra oversettelsesfilene.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4449 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/group_schedule.rb
+++ app/models/group_schedule.rb
@@ -8,10 +8,7 @@ class GroupSchedule < ActiveRecord::Base
   validates_presence_of :end_at, :group, :start_at, :weekday
 
   def weekday_name
-    {
-        1 => 'Mandag', 2 => 'Tirsdag', 3 => 'Onsdag', 4 => 'Torsdag', 5 => 'Fredag', 6 => 'Lørdag',
-        7 => 'Søndag'
-    }[weekday]
+    I18n.t(:date)[:day_names][weekday]
   end
 
   def next_practice
