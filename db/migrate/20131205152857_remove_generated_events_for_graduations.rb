106e53ec8de95b448f18d3dbebb87630d75ac8cf
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2013-12-06 06:31:15 +0000

    * Fjernet generert Event for Graduation siden den ikke tilfører noe informasjon. * Vise gruppe for gradering.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4513 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

---
+++ db/migrate/20131205152857_remove_generated_events_for_graduations.rb
@@ -0,0 +1,20 @@
+class RemoveGeneratedEventsForGraduations < ActiveRecord::Migration
+  def up
+    Graduation.all.each do |g|
+      g.event.destroy
+    end
+    remove_column :graduations, :event_id
+  end
+
+  def down
+    remove_column :graduations, :event_id, :null => false
+  end
+
+  class Graduation < ActiveRecord::Base
+    belongs_to :event
+  end
+
+  class Event < ActiveRecord::Base
+
+  end
+end
