42db59d9d45694007f609254d726cdacf6c13f7d
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2014-02-02 10:25:16 +0000

    * Sikre at det bare er et årsmøte per år.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4544 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/annual_meeting.rb
+++ app/models/annual_meeting.rb
@@ -2,7 +2,9 @@ class AnnualMeeting < ActiveRecord::Base
   attr_accessible :invitation_sent_at, :public_record_updated_at, :start_at
 
   validate do
-    if start_at && self.class.where('EXTRACT(YEAR FROM start_at) = ?', start_at.year).exists?
+    if id && start_at && self.class.
+        where('id <> ? AND EXTRACT(YEAR FROM start_at) = ?', id, start_at.year).
+        exists?
       errors.add(:start_at, 'kan bare ha et årsmøte per år.')
     end
   end
