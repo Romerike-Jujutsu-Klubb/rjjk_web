1ade27517c28a5ad306db75996173cf9484d7046
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2014-09-11 08:00:15 +0000

    * Tillat gamle sessions.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4626 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,27 +4,21 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
   config.action_mailer.default_url_options = {host: 'example.com'}
   config.action_mailer.delivery_method = :test
-
-  # config.active_record.mass_assignment_sanitizer = :strict
-
   config.active_support.deprecation = :stderr
   config.cache_classes = true
   config.consider_all_requests_local = true
   config.eager_load = true
   config.serve_static_assets = true
   config.static_cache_control = 'public, max-age=3600'
-
-  # config.whiny_nils = true
-
   config.after_initialize { Timecop.freeze(Time.local 2013, 10, 17, 18, 46, 0) } # Week 42
 
-  # config.middleware.use ExceptionNotification::Rack, {
-  #     :ignore_exceptions => [],
-  #     :email => {
-  #         :email_prefix => "[RJJK][#{Rails.env}] ",
-  #         :sender_address => "\"RJJK #{Rails.env} Exception Notifier\" <noreply-#{Rails.env}@jujutsu.no>",
-  #         :exception_recipients => %w{uwe@kubosch.no}
-  #     }
-  # }
+  config.middleware.use ExceptionNotification::Rack, {
+      :ignore_exceptions => [],
+      :email => {
+          :email_prefix => "[RJJK][#{Rails.env}] ",
+          :sender_address => "\"RJJK #{Rails.env} Exception Notifier\" <noreply-#{Rails.env}@jujutsu.no>",
+          :exception_recipients => %w{uwe@kubosch.no}
+      }
+  }
 
 end
