e19bd8290e7501d54fee8711812403ab925a31d0
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2014-12-10 10:34:54 +0000

    * Rettet rute til gradering.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4711 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   match 'attendances/:action/:year/:week/:group_schedule_id/:status(/:member_id)',
       controller: :attendances, via: [:get, :post]
   get 'attendances/form/:year/:month/:group_id' => 'attendances#form'
-  get ':controller/report(/:year/:month)', action: :report
+  patch 'graduations/add_group/:id' => 'graduations#add_group'
   get 'info/versjon'
   get 'members/search'
-  get 'members/grade_history_graph/:id(/:percentage/:step/:interval).:format' => 'members#grade_history_graph'
-  get ':controller/history_graph/:id.:format', action: :history_graph
+  get 'members/:action/:id(/:percentage/:step/:interval).:format' => 'members'
   get 'mitt/oppmote' => 'attendances#plan', as: :attendance_plan
   get 'attendances/plan' # må være etter mitt/oppmote
   post 'attendances/announce(/:year/:week/:group_schedule_id)/:status(/:member_id)' =>
@@ -15,6 +14,10 @@ Rails.application.routes.draw do
   get 'pensum' => 'ranks#pensum'
   get 'pensum/pdf' => 'ranks#pdf'
   get 'search/index'
+  get 'svar/:id', controller: :survey_requests, action: :answer_form
+  patch 'svar/:id', controller: :survey_requests, action: :save_answers
+  post 'svar/:id', controller: :survey_requests, action: :save_answers
+  get 'takk/:id', controller: :survey_requests, action: :thanks
   get 'user/change_password'
   post 'user/change_password'
   get 'user/forgot_password'
@@ -25,19 +28,21 @@ Rails.application.routes.draw do
   post 'user/signup'
   get 'user/welcome'
 
-  get ':controller/month_per_year_chart/:month/:size.:format', action: :month_per_year_chart
   get ':controller/active_contracts', action: :active_contracts
-  match ':controller/form', action: :form, via: [:get, :post]
-  match ':controller/form/:id', action: :form, via: [:get, :post]
-  match ':controller/form_index', action: :form_index, via: [:get, :post]
   get ':controller/calendar', action: :calendar
   post ':controller/create', action: :create
   get ':controller/email_list', action: :email_list
   get ':controller/excel_export', action: :excel_export
+  match ':controller/form', action: :form, via: [:get, :post]
+  match ':controller/form/:id', action: :form, via: [:get, :post]
+  match ':controller/form_index', action: :form_index, via: [:get, :post]
   get ':controller/gallery(/:id)', action: :gallery
+  get ':controller/history_graph/:id.:format', action: :history_graph
   get ':controller/list_active', action: :list_active
   get ':controller/list_inactive', action: :list_active
   get ':controller/mine(/:id)', action: :mine
+  get ':controller/month_per_year_chart/:month/:size.:format', action: :month_per_year_chart
+  get ':controller/report(/:year/:month)', action: :report
   get ':controller/telephone_list', action: :telephone_list
   get ':controller/yaml', action: :yaml
 
@@ -82,6 +87,11 @@ Rails.application.routes.draw do
   resources :roles
   resources :semesters
   resources :signatures
+  resources :survey_answer_translations
+  resources :survey_answers
+  resources :survey_questions
+  resources :survey_requests
+  resources :surveys
   resources :technique_applications
   resources :trial_attendances
   resources :users, controller: :user, path: :user
