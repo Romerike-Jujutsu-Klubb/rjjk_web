738614b0e22428563efca9adf84de318d2cc20ed
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2015-05-02 16:26:51 +0000

    Innkommende e-post innhold er påkrevet
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4838 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- db/migrate/20150502114322_create_raw_incoming_emails.rb
+++ db/migrate/20150502114322_create_raw_incoming_emails.rb
@@ -1,7 +1,7 @@
 class CreateRawIncomingEmails < ActiveRecord::Migration
   def change
     create_table :raw_incoming_emails do |t|
-      t.binary :content
+      t.binary :content, null: false
 
       t.timestamps
     end
