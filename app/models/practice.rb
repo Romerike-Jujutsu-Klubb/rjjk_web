8c41f05afc311225d7857384efea3911bded42ac
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2015-05-30 06:44:06 +0000

    Syntaks endring.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@4868 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,10 +1,10 @@
 class Practice < ActiveRecord::Base
   belongs_to :group_schedule
 
-  has_many :attendances, :dependent => :destroy
-  has_many :trial_attendances, :dependent => :destroy
+  has_many :attendances, dependent: :destroy
+  has_many :trial_attendances, dependent: :destroy
 
-  validates_uniqueness_of :group_schedule_id, :scope => [:year, :week]
+  validates_uniqueness_of :group_schedule_id, scope: [:year, :week]
 
   def date
     Date.commercial(year, week, group_schedule.weekday)
