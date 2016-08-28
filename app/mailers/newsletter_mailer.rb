da84321b71a75e02d14de2d405db063e3bad921e
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2016-08-28 18:58:04 +0000

    Vis bare dato for nyheter.
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@5067 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -20,7 +20,7 @@ class NewsletterMailer < ApplicationMailer
   def newsletter(news_item, member)
     @news_item = news_item
     @user_email = member.user&.email
-    @timestamp = @news_item.updated_at
+    @timestamp = @news_item.updated_at.to_date
     @email_url = { controller: :news, action: :show, id: @news_item.id }
     mail to: member.email, subject: @news_item.title
   end
