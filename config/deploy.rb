5df26d827dc22d74ab5f32217bfd6cff95e3c324
donv <donv@c3fb086e-ec1b-0410-9d0f-f56cd9c64383>
2016-09-05 06:36:33 +0000

    Use 2G heap for rake tasks
    git-svn-id: svn+ssh://source.kubosch.no/var/svn/trunk/rjjk_web@5076 c3fb086e-ec1b-0410-9d0f-f56cd9c64383

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '~>3.4'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'public/attendances',
     'public/images', 'public/members', 'tmp')
-set :default_env, JRUBY_OPTS: '--dev', DISABLE_SCHEDULER: true
+set :default_env, JRUBY_OPTS: '--dev -J-Xmx2G', DISABLE_SCHEDULER: true
 set :rvm_ruby_version, File.read(File.expand_path('../.ruby-version', File.dirname(__FILE__))).strip
 set :pty, true
 
