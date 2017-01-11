a5fd14e76e3190e6d5f985b7f8ee124febbb1ee2
donv <uwe@kubosch.no>
2017-01-11 17:51:24 +0100

    Use git for deployment to production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 set :application, 'rjjk_web'
-set :scm, :svn
-set :repo_url, "svn+ssh://capistrano@kubosch.no/var/svn/trunk/#{fetch :application}"
+set :scm, :git
+set :repo_url, "https://github.com/Romerike-Jujutsu-Klubb/#{fetch :application}.git"
 set :deploy_to, -> { "/u/apps/#{fetch :application}" }
 set :keep_releases, 30
 
