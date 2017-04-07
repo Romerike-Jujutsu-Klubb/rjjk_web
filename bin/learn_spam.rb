c7d123026d94f08964ce3de750a8eb996a108652
donv <uwe@kubosch.no>
2017-04-07 12:21:59 +0200

    Do a non-interactive pass before the interactive one

--- bin/learn_spam.rb
+++ bin/learn_spam.rb
@@ -17,7 +17,7 @@ def check_if_spam(escaped_filename)
   `spamc < #{escaped_filename} | grep 'X-Spam-Status'`
 end
 
-def learn(escaped_filename, type)
+def learn_file(escaped_filename, type)
   system(%(sa-learn -u capistrano --#{type} "#{escaped_filename}")) || raise('learning spam failed')
 end
 
@@ -45,12 +45,6 @@ def text_from_part(m)
   end
 end
 
-Time.zone = 'Copenhagen'
-Dir.chdir File.expand_path '..', __dir__
-
-files = Dir['mail_*']
-sorted = files.sort_by { |f| f[0..24] }
-
 def check_and_learn_if_spam(f, escaped_filename)
   skip = false
   new_spam_status = check_if_spam(escaped_filename)
@@ -58,67 +52,84 @@ def check_and_learn_if_spam(f, escaped_filename)
     print "[#{spam_score}] : ".rjust(PROMPT_COLUMN - f.size, ' ')
     if spam_score.to_f >= SPAM_AUTOLEARN_LIMIT
       puts 'LEARNING'
-      learn(escaped_filename, 'spam')
+      learn_file(escaped_filename, 'spam')
       skip = true
     end
   end
   skip
 end
 
-sorted.each.with_index do |f, i|
-  if f.valid_encoding?
-    escaped_filename = Shellwords.escape(f)
-    loop do
-      print "[#{files.size - i}] #{f.gsub(/^mail_/, '')}"
+def learn(interactive)
+  sorted = Dir['mail_*'].sort_by { |f| f[0..24] }
 
-      file_date = Time.zone.parse(f[5..14] + 'T' + f[16..23])
-      if file_date < 2.days.ago
-        puts ': OLD'.rjust(PROMPT_COLUMN - f.size, ' ')
-        break
-      end
+  sorted.each.with_index do |f, i|
+    if f.valid_encoding?
+      escaped_filename = Shellwords.escape(f)
+      delete = loop do
+        print "[#{sorted.size - i}] #{f.gsub(/^mail_/, '')}"
 
-      /\[SPAM\]\[(?<old_spam_score>\d+\.\d+)\]/ =~ f
-      if old_spam_score
-        break if check_and_learn_if_spam(f, escaped_filename)
-      elsif /______\[(?<old_ham_score>-\d+\.\d+)\]/ =~ f
-        print ': '.rjust(PROMPT_COLUMN - f.size, ' ')
-        if old_ham_score.to_f <= -2.0
-          puts 'HAM'
-          break
+        file_date = Time.zone.parse(f[5..14] + 'T' + f[16..23])
+        if file_date < 2.days.ago
+          puts ': OLD'.rjust(PROMPT_COLUMN - f.size, ' ')
+          break true
         end
-      else
-        print ': '.rjust(PROMPT_COLUMN - f.size, ' ')
-      end
 
-      q = gets.chomp
-      if q == 's'
-        learn(escaped_filename, 'spam')
-        break
-      elsif q == 'h'
-        learn(escaped_filename, 'ham')
-        break
-      elsif q == 'd'
-        print "\e[3J"
-        m = Mail.read(f)
-        puts "From: #{m['from']}"
-        puts "To: #{m['to']}"
-        puts "Subject: #{m['subject']}"
-        puts
-        puts text_from_part(m)
-        puts
-        puts
-      elsif q == 'c'
-        break if check_and_learn_if_spam(f, escaped_filename)
-      elsif q == ''
-        puts if i == sorted.size - 1
-        break
+        /\[SPAM\]\[(?<old_spam_score>\d+\.\d+)\]/ =~ f
+        if old_spam_score
+          break true if check_and_learn_if_spam(f, escaped_filename)
+        elsif /______\[(?<old_ham_score>-\d+\.\d+)\]/ =~ f
+          print ': '.rjust(PROMPT_COLUMN - f.size, ' ')
+          if old_ham_score.to_f <= -2.0
+            puts 'HAM'
+            break true
+          end
+        else
+          print ': '.rjust(PROMPT_COLUMN - f.size, ' ')
+        end
+
+        unless interactive
+          puts
+          break false
+        end
+
+        q = gets.chomp
+        if q == 's'
+          learn_file(escaped_filename, 'spam')
+          break true
+        elsif q == 'h'
+          learn_file(escaped_filename, 'ham')
+          break true
+        elsif q == 'd'
+          print "\e[3J"
+          m = Mail.read(f)
+          puts "From: #{m['from']}"
+          puts "To: #{m['to']}"
+          puts "Subject: #{m['subject']}"
+          puts
+          puts text_from_part(m)
+          puts
+          puts
+        elsif q == 'c'
+          break true if check_and_learn_if_spam(f, escaped_filename)
+        elsif q == ''
+          puts if i == sorted.size - 1
+          break true
+        end
       end
+    else
+      puts "[#{sorted.size - i}] #{f} [BAD ENCODING]"
+      delete = true
     end
-  else
-    puts "[#{files.size - i}] #{f} [BAD ENCODING]"
-  end
 
-  File.delete f
+    File.delete(f) if delete
+  end
 end
+
+Time.zone = 'Copenhagen'
+Dir.chdir File.expand_path '..', __dir__
+
+learn(false)
+puts
+learn(true)
 
 system(%(sa-learn --sync)) || raise('syncing learned spam failed')
