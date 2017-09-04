d467bb8f8b71956a84cc3aca7b74c54968653400
donv <uwe@kubosch.no>
2017-09-05 00:08:56 +0200

    Fix "Remember me"

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,9 +3,10 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_rjjk_web_session', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_rjjk_web_session',
+      domain: :all, tld_length: 2
 elsif Rails.env.beta?
-  Rails.application.config.session_store :cookie_store, key: '_rjjk_web_beta_session',
+  Rails.application.config.session_store :cookie_store, key: '_rjjk_web_session',
       domain: 'beta.jujutsu.no'
 else
   Rails.application.config.session_store :cookie_store, key: '_rjjk_web_session'
