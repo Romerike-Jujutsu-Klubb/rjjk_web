f9918ac543fb9abb482cbaafea63cfc547ae87f9
donv <uwe@kubosch.no>
2017-09-05 14:21:48 +0200

    Fix JS updates for graduation candidates

--- app/controllers/censors_controller.rb
+++ app/controllers/censors_controller.rb
@@ -44,7 +44,7 @@ class CensorsController < ApplicationController
     @censor.attributes = params[:censor]
     return unless admin_or_censor_required(@censor.graduation)
     if @censor.save
-      flash[:notice] = "La til **#{@censor.member.name}** som #{@censor.examiner ? 'eksaminator' : 'sensor'} på graderingen."
+      flash[:notice] = "La til **#{@censor.member.name}** som #{@censor.role_name} på graderingen."
       back_or_redirect_to action: :index
     else
       render action: :new
