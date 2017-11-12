a9fb429964121d8b8c26308e06995ad352ba9bff
donv <uwe@kubosch.no>
2017-11-12 12:05:02 +0100

    New applicatons

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-env_prefix = ("#{Rails.env}." unless Rails.env.production?)
-Rails.application.config.middleware.use ExceptionNotification::Rack,
-    ignore_exceptions: [],
-    email: {
-      email_prefix: "[RJJK]#{"[#{Rails.env.upcase}]" unless Rails.env.production?} ",
-      sender_address:
-            "\"RJJK #{Rails.env.upcase} Exception Notifier\" <noreply@#{env_prefix}jujutsu.no>",
-      exception_recipients: %w[uwe@kubosch.no],
-    }
+if Rails.env.production? || Rails.env.beta?
+  env_prefix = ("#{Rails.env}." unless Rails.env.production?)
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+      ignore_exceptions: [],
+      email: {
+        email_prefix: "[RJJK]#{"[#{Rails.env.upcase}]" unless Rails.env.production?} ",
+        sender_address:
+              "\"RJJK #{Rails.env.upcase} Exception Notifier\" <noreply@#{env_prefix}jujutsu.no>",
+        exception_recipients: %w[uwe@kubosch.no],
+      }
+end
