7e02204231768c6a3a239912faf4ac175130e494
donv <uwe@kubosch.no>
2017-11-27 22:51:03 +0100

    Avoid cluster mode in test and development environments

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,8 @@ threads_count = ENV.fetch('RAILS_MAX_THREADS') { RUBY_ENGINE == 'jruby' ? 20 : 5
 threads threads_count, threads_count
 port ENV.fetch('PORT') { 3000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
-workers ENV.fetch('WEB_CONCURRENCY') { 2 } if RUBY_ENGINE == 'ruby'
+if ['production', 'beta'].include?(ENV['RAILS_ENV']) && RUBY_ENGINE == 'ruby'
+  workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+end
 prune_bundler
 plugin :tmp_restart
