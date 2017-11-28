3d58d04b322dfe2b18eff86cfef6ea2d2ca74ce1
donv <uwe@kubosch.no>
2017-11-28 18:35:54 +0100

    Take a screenshot of the front

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,8 +3,12 @@
 require 'feature_test'
 
 class CanAccessHomeTest < FeatureTest
-  test 'sanity' do
+  setup {screenshot_section :home}
+
+  test 'front' do
+    screenshot_group :front
     visit root_path
     assert_selector 'h1', text: 'Velkommen'
+    screenshot :index
   end
 end
