c66d3457e15deb4747c5566ce1d44fc46622b6e0
donv <uwe@kubosch.no>
2017-11-28 11:18:42 +0100

    Use a word array when possible

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@ threads_count = ENV.fetch('RAILS_MAX_THREADS') { RUBY_ENGINE == 'jruby' ? 20 : 5
 threads threads_count, threads_count
 port ENV.fetch('PORT') { 3000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
-if ['production', 'beta'].include?(ENV['RAILS_ENV']) && RUBY_ENGINE == 'ruby'
+if %w[production beta].include?(ENV['RAILS_ENV']) && RUBY_ENGINE == 'ruby'
   workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 end
 prune_bundler
