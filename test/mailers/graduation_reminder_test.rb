ceb268e60025697c5f5bdd25357c851c75832d00
donv <uwe@kubosch.no>
2017-11-29 17:10:15 +0100

    Adjust test to new template

--- test/mailers/graduation_reminder_test.rb
+++ test/mailers/graduation_reminder_test.rb
@@ -50,7 +50,7 @@ class GraduationReminderTest < ActionMailer::TestCase
     assert_equal %w[noreply@test.jujutsu.no], mail.from
     assert_equal 'Gradering for Voksne 2013-10-24', mail.subject
     assert_match(/Hei Lars!/, mail.body)
-    assert_match(/Felles gradering for Voksne er satt opp til 2013-10-24./, mail.body)
+    assert_match('Felles gradering for Voksne er satt opp til <b>2013-10-24</b>.', mail.body)
     assert_match(%r{Din nåværende registrerte grad hos oss er <b>1. kyu brunt belte</b>.}, mail.body)
     assert_match(%r{Din neste grad er <b>shodan svart belte</b>.}, mail.body)
 
@@ -59,7 +59,7 @@ class GraduationReminderTest < ActionMailer::TestCase
     assert_equal %w[noreply@test.jujutsu.no], mail.from
     assert_equal 'Gradering for Voksne 2013-10-24', mail.subject
     assert_match(/Hei Newbie!/, mail.body)
-    assert_match(/Felles gradering for Voksne er satt opp til 2013-10-24./, mail.body)
+    assert_match('Felles gradering for Voksne er satt opp til <b>2013-10-24</b>.', mail.body)
     assert_match(/Vi har ikke registrert noen grad for deg fra før./,
         mail.body)
     assert_match(%r{Din neste grad er <b>5. kyu gult belte</b>.}, mail.body)
@@ -69,7 +69,7 @@ class GraduationReminderTest < ActionMailer::TestCase
     assert_equal %w[noreply@test.jujutsu.no], mail.from
     assert_equal 'Gradering for Voksne 2013-10-24', mail.subject
     assert_match(/Hei Uwe!/, mail.body)
-    assert_match(/Felles gradering for Voksne er satt opp til 2013-10-24./, mail.body)
+    assert_match('Felles gradering for Voksne er satt opp til <b>2013-10-24</b>.', mail.body)
     assert_match(%r{Din nåværende registrerte grad hos oss er <b>nidan svart belte m/2 striper</b>.},
         mail.body)
     assert_match(%r{Din neste grad er <b>sandan svart belte m/3 striper</b>.}, mail.body)
