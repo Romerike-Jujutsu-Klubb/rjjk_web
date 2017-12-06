f47a4b01e47f626134b4e44f6b0699cfdfa83fac
donv <uwe@kubosch.no>
2017-12-06 13:12:20 +0100

    grammar...

--- app/services/graduation_reminder.rb
+++ app/services/graduation_reminder.rb
@@ -109,7 +109,7 @@ class GraduationReminder
 
   def self.send_shopping_list
     Graduation.upcoming
-        .ready(1.days.ago)
+        .ready(1.day.ago)
         .where('graduations.held_on < ?', 10.days.from_now)
         .where(shopping_list_sent_at: nil)
         .order('graduations.held_on')
