db079677b61a96fee507838b220f22abed43572c
donv <uwe@kubosch.no>
2017-12-21 23:29:21 +0100

    Rubocop Lint/UnneededDisable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  include UserSystem # rubocop: disable Style/MixinUsage
+  include UserSystem
 
   def menu_item(name, options = {})
     options[:controller] ||= 'info'
