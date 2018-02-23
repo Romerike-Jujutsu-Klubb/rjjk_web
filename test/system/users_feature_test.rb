2d1df7d748bf6766aae6bf7782692eff9467817a
donv <uwe@kubosch.no>
2018-02-23 19:55:26 +0100

    Send login link

--- test/system/users_feature_test.rb
+++ test/system/users_feature_test.rb
@@ -16,4 +16,11 @@ class UsersFeatureTest < FeatureTest
     visit users_path
     screenshot(:index)
   end
+
+  def test_send_login_link
+    visit_with_login user_path(users(:lars))
+    click_link 'Send e-post med login link'
+    assert_current_path root_path
+    find '.alert-warning p', text: 'En e-post med innloggingslenke er sendt til lars@example.com.'
+  end
 end
