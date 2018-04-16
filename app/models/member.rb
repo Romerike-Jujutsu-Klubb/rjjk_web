a8743db4f5965a3a7331eb93e2b653f500826075
donv <uwe@kubosch.no>
2018-04-16 21:03:24 +0200

    Synchronize contact email

--- app/models/member.rb
+++ app/models/member.rb
@@ -61,8 +61,8 @@ class Member < ApplicationRecord
   end
   scope :search, ->(query) do
     includes(:nkf_member).references(:nkf_members)
-    .where(SEARCH_FIELDS.map { |c| "UPPER(#{c}::text) ~ :query" }.join(' OR '),
-        query: UnicodeUtils.upcase(query).split(/\s+/).join('|'))
+        .where(SEARCH_FIELDS.map { |c| "UPPER(#{c}::text) ~ :query" }.join(' OR '),
+            query: UnicodeUtils.upcase(query).split(/\s+/).join('|'))
         .order(:first_name, :last_name)
   end
 
--- db/migrate/20180409133742_add_parent2_email_to_members.rb
+++ db/migrate/20180409133742_add_parent2_email_to_members.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddParent2EmailToMembers < ActiveRecord::Migration[5.1]
   def change
     add_column :members, :parent_2_email, :string, limit: 64
