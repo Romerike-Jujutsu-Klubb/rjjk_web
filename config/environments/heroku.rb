7922b80a729e86534af2aa6d47afa3a37f58d78f
donv <uwe@kubosch.no>
2018-09-19 12:55:41 +0200

    Rubocop cleanup

--- config/environments/heroku.rb
+++ config/environments/heroku.rb
@@ -6,17 +6,17 @@ Rails.application.configure do
   config.action_mailer.asset_host = 'https://jujutsu-no.herokuapp.com'
   config.action_mailer.default_url_options = { protocol: 'https', host: 'jujutsu-no.herokuapp.com' }
 
- # Setup the mailer config
+  # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'jujutsu.no',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'jujutsu.no',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true,
   }
 
   config.log_level = :debug
