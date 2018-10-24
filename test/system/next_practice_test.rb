d6ef9e7c9a6eb91259304e52f9bbe7c64707c1bf
donv <uwe@kubosch.no>
2018-10-24 23:54:53 +0200

    avoid screenshots since they freeze the tests...

--- test/system/next_practice_test.rb
+++ test/system/next_practice_test.rb
@@ -8,21 +8,21 @@ class NextPracticeTest < ApplicationSystemTestCase
     screenshot_group :next_practice
     visit root_url
     assert_no_selector 'h4', text: 'Neste trening'
-    screenshot :anonymous
+    # screenshot :anonymous
     login
     assert_selector 'h4', text: 'Neste trening'
     assert_text 'Du kommer.'
-    screenshot :logged_in
+    # screenshot :logged_in
 
     practice = practices(:voksne_2013_42_thursday)
     practice.attendances.create! member_id: id(:lars), status: 'X'
     practice.attendances.create! member_id: id(:newbie), status: 'X'
     visit root_url
-    screenshot :with_others
+    # screenshot :with_others
 
     click_on 'Du og 2 andre'
     assert_selector 'h5', text: 'Påmeldt'
     assert_selector 'li', text: 'Uwe Kubosch'
-    screenshot :modale
+    # screenshot :modale
   end
 end
