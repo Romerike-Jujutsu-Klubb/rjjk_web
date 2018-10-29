3f3f441993785de4caaee1de71407136310e09e3
donv <uwe@kubosch.no>
2018-10-29 22:23:16 +0100

    add more standard puma config

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,10 @@ threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 port ENV.fetch('PORT') { 3000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
-workers ENV.fetch('WEB_CONCURRENCY') { 2 } if %w[production heroku beta].include?(get(:environment))
-prune_bundler
+workers ENV.fetch('WEB_CONCURRENCY') { 2 } if %w[production beta].include?(get(:environment))
+preload_app!
 plugin :tmp_restart
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
