306cc4564bd6f92c3eb71a6321b515046a9d653b
donv <uwe@kubosch.no>
2018-11-15 14:25:44 +0100

    Skip resetting cookies for speed

--- test/system_test_helper.rb
+++ test/system_test_helper.rb
@@ -35,22 +35,7 @@ module SystemTestHelper
       find('.alert button.close').click
       open_menu
       find('a', text: 'Logg inn')
-      # FIXME(uwe): Is this necessary?
       Capybara.reset_sessions!
-      clear_cookies
-    end
-  end
-
-  def clear_cookies
-    browser = Capybara.current_session.driver.browser
-    if browser.respond_to?(:clear_cookies)
-      # Rack::MockSession
-      browser.clear_cookies
-    elsif browser.respond_to?(:manage) && browser.manage.respond_to?(:delete_all_cookies)
-      # Selenium::WebDriver
-      browser.manage.delete_all_cookies
-    else
-      raise "Don't know how to clear cookies. Weird driver?"
     end
   end
 
