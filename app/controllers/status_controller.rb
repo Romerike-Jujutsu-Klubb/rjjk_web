d71869d576abc00931acc705f1d64f59117d2f05
donv <uwe@kubosch.no>
2018-11-18 23:25:33 +0100

    Sort heap dumps

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -8,7 +8,7 @@ class StatusController < ApplicationController
   HEAP_DUMP_SUFFIX = '.json'
 
   def index
-    @heap_dumps = Dir["#{HEAP_DUMP_PATH}/#{HEAP_DUMP_PREFIX}*#{HEAP_DUMP_SUFFIX}"]
+    @heap_dumps = Dir["#{HEAP_DUMP_PATH}/#{HEAP_DUMP_PREFIX}*#{HEAP_DUMP_SUFFIX}"].sort
         .map { |f| [f.split('/').last, File.size(f)] }
   end
 
