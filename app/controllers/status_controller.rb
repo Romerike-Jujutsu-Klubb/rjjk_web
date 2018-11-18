b004afbe97694e5fea09bbf9e8847189269c59aa
donv <uwe@kubosch.no>
2018-11-18 19:33:43 +0100

    Dump heap to file

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class StatusController < ApplicationController
+  PUBLIC_FILE = '/heap.json'
+
   def index; end
 
   def heap_dump
-    render plain: ObjectSpace.dump_all(output: :string)
+    File.open("#{Rails.root}/public#{PUBLIC_FILE}", 'w') { |file| ObjectSpace.dump_all(output: file) }
+    redirect_to PUBLIC_FILE
   end
 end
