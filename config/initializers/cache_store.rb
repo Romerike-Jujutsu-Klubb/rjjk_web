a1d49ed9bd519e5b4cf06b2488f71e9781acd6fa
donv <uwe@kubosch.no>
2018-12-01 19:13:33 +0100

    More explicit env var value

--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-if ENV['LOG_CACHE_STORE'] || Rails.env.development?
+if Rails.env.development? || ENV['LOG_CACHE_STORE'] == 'ENABLED'
   ActiveSupport::Cache::Store.logger = Rails.logger
 end
