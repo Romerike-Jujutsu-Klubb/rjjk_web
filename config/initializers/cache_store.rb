389097c7c84044d13a766ea35dc3a61f69025702
donv <uwe@kubosch.no>
2018-12-01 21:49:51 +0100

    Enable cache logging

--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env.development? || ENV['CACHE_STORE_LOGGER'] == 'ENABLED'
-  ActiveSupport::Cache::Store.logger = Rails.logger
+  ActiveSupport::Cache::Store.logger = Rails.logger.dup
+  ActiveSupport::Cache::Store.logger.level = :debug
+  ActiveSupport::Cache::Store.logger.debug 'Cache store logger enabled.'
 end
