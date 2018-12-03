4fc143d022dea9c2287bb8b93c7e59447f8b0a8c
donv <uwe@kubosch.no>
2018-12-03 02:23:07 +0100

    Set max-age HTTP header for better caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   config.lograge.enabled = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{1.year.to_i}",
+  }
   config.require_master_key = true
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger = ActiveSupport::Logger.new(STDOUT)
