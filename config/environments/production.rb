969598c361b33d807eaeffcd5b45399de1a63f1d
donv <uwe@kubosch.no>
2018-12-27 13:28:03 +0100

    Try using WebHuset for email sending again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,10 +11,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { protocol: 'https', host: 'jujutsu.no' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    # user_name: ENV['SENDGRID_USERNAME'],
+    # password: ENV['SENDGRID_PASSWORD'],
+    # address: 'smtp.sendgrid.net',
+    user_name: ENV["SMTP_USERNAME_#{Date.current.cwday}"] || ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    address: 'smtp.webhuset.no',
     domain: 'jujutsu.no',
-    address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true,
