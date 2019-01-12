004e3e6b4f6ef02fa9a3f36d36399ae32b9825b0
donv <uwe@kubosch.no>
2019-01-12 17:23:03 +0100

    inline max_age

--- app/models/member_age_chart.rb
+++ app/models/member_age_chart.rb
@@ -4,8 +4,7 @@ class MemberAgeChart
   def self.data_set(timestamp = Member.maximum(:updated_at).strftime('%F_%T.%N'))
     Rails.cache.fetch(age_group_key(timestamp)) do
       members = Member.active(Date.current).select { |m| m.paying? || m.active? }
-      member_ages = members.map(&:age)
-      max_age = ((member_ages.max / 5.0).ceil * 5)
+      max_age = ((members.map(&:age).max / 5.0).ceil * 5)
       age_groups = [0..5, 6..9, 10..14, 15..19, 20..24, 25..29, 30..39, 40..49, 50..max_age]
       age_data = age_groups.map { |age_range| members.select { |m| age_range.cover? m.age } }
       age_groups = age_groups.map(&:to_s).zip(age_data)
