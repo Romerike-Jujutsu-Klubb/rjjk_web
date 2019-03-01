8646b3687951361d66aa98ddc97e5a2d9e48e217
donv <uwe@kubosch.no>
2019-03-01 17:00:19 +0100

    Display index after updating a page alias

--- app/controllers/page_aliases_controller.rb
+++ app/controllers/page_aliases_controller.rb
@@ -5,29 +5,14 @@ class PageAliasesController < ApplicationController
 
   def index
     @page_aliases = PageAlias.order(:old_path, :new_path).to_a
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @page_aliases }
-    end
   end
 
   def show
     @page_alias = PageAlias.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @page_alias }
-    end
   end
 
   def new
     @page_alias = PageAlias.new
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @page_alias }
-    end
   end
 
   def edit
@@ -36,39 +21,25 @@ class PageAliasesController < ApplicationController
 
   def create
     @page_alias = PageAlias.new(params[:page_alias])
-
-    respond_to do |format|
-      if @page_alias.save
-        format.html { redirect_to @page_alias, notice: 'Page alias was successfully created.' }
-        format.json { render json: @page_alias, status: :created, location: @page_alias }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @page_alias.errors, status: :unprocessable_entity }
-      end
+    if @page_alias.save
+      redirect_to page_aliases_path, notice: 'Page alias was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
   def update
     @page_alias = PageAlias.find(params[:id])
-
-    respond_to do |format|
-      if @page_alias.update(params[:page_alias])
-        format.html { redirect_to @page_alias, notice: 'Page alias was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @page_alias.errors, status: :unprocessable_entity }
-      end
+    if @page_alias.update(params[:page_alias])
+      redirect_to page_aliases_path, notice: 'Page alias was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   def destroy
     @page_alias = PageAlias.find(params[:id])
     @page_alias.destroy
-
-    respond_to do |format|
-      format.html { redirect_to page_aliases_url }
-      format.json { head :no_content }
-    end
+    redirect_to page_aliases_url
   end
 end
