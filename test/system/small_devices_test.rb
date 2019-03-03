ad6e60a0ef8b7e1e77e8699388101992f5ae0d6b
donv <uwe@kubosch.no>
2019-03-03 17:41:20 +0100

    Wait for scroll to complete

--- test/system/small_devices_test.rb
+++ test/system/small_devices_test.rb
@@ -54,6 +54,7 @@ class SmallDevicesTest < ApplicationSystemTestCase
     visit front_page_path
     assert_css('.fa-chevron-down')
     find('.fa-chevron-down').click
+    sleep 0.5 # Wait for scroll to complete
     assert_css('#footer .menu-item a', text: 'MY FIRST ARTICLE')
     screenshot :scrolled
     find('#footer .menu-item a', text: 'MY FIRST ARTICLE').click
