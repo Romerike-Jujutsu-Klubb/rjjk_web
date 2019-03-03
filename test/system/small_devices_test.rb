0d235b2276d178cd25baa30ae1bacc31634df468
donv <uwe@kubosch.no>
2019-03-03 23:49:14 +0100

    `visible: true` is the default

--- test/system/small_devices_test.rb
+++ test/system/small_devices_test.rb
@@ -40,7 +40,7 @@ class SmallDevicesTest < ApplicationSystemTestCase
   test 'new front_page' do
     screenshot_group :new_front_page
     visit front_page_path
-    assert_css('#headermenuholder > i', visible: true)
+    assert_css('#headermenuholder > i')
     screenshot :index, color_distance_limit: 11
     find('#headermenuholder > i').click
     assert_selector '.menubutton', text: 'My first article'
