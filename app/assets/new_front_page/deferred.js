a72e84646231f2acfba4fdc96f365dbb68296ef0
donv <uwe@kubosch.no>
2019-03-12 10:01:43 +0100

    Remove unused javascript

--- app/assets/new_front_page/deferred.js
+++ app/assets/new_front_page/deferred.js
@@ -3,20 +3,15 @@
 //= require core.min
 //= require mouse.min
 //= require slider.min
-//= require accordion.min
 //= require sortable.min
-//= require draggable.min
-//= require jquery.form.min
 //= require jquery.themepunch.plugins.min
 //= require bootstrap.min
 //= require jquery.themepunch.punchbox
-//= require jquery.magnific-popup
 //= require scrollspy
 //= require scrollspyjquery
 //= require my_custom
 
 var goodweb_theme_vars = {"load": "LOAD", "more": "MORE"};
 
-//= require comment-reply.min
 //= require cf-landing-page
 //= require wp-embed.min
