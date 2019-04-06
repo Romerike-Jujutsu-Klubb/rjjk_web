a50493de3d37c5594c45559978cb526537f11215
donv <uwe@kubosch.no>
2019-04-06 17:45:24 +0200

    Improve test coverage

--- test/models/event_invitee_message_test.rb
+++ test/models/event_invitee_message_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class EventInviteeMessageTest < ActiveSupport::TestCase
-  test 'the truth' do
-    assert true
+  test 'initiaslize SIGNUP_CONFIRMATION' do
+    EventInviteeMessage.new(message_type: EventInviteeMessage::MessageType::SIGNUP_CONFIRMATION,
+    event_invitee: event_invitees(:one))
+  end
+  test 'initiaslize SIGNUP_REJECTION' do
+    EventInviteeMessage.new(message_type: EventInviteeMessage::MessageType::SIGNUP_REJECTION,
+    event_invitee: event_invitees(:one))
   end
 end
