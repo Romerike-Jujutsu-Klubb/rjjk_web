ce9e6bce70c39c25c5cab408722982e94ab32377
donv <uwe@kubosch.no>
2019-04-10 01:47:26 +0200

    Better test coverage

--- test/mailers/event_mailer_test.rb
+++ test/mailers/event_mailer_test.rb
@@ -3,6 +3,22 @@
 require 'test_helper'
 
 class NewsletterMailerTest < ActionMailer::TestCase
+  test 'event_invitation' do
+    mail = EventMailer.event_invitation(events(:one), 'test@example.com')
+    assert_equal 'Invitasjon til THE EVENT', mail.subject
+    assert_equal ['test@example.com'], mail.to
+    assert_equal %w[test@jujutsu.no], mail.from
+    mail.deliver_now
+  end
+
+  test 'registration_confirmation' do
+    mail = EventMailer.registration_confirmation(event_invitees(:one))
+    assert_equal 'Bekreftelse av påmelding til THE EVENT', mail.subject
+    assert_equal ['newbie@example.com'], mail.to
+    assert_equal %w[test@jujutsu.no], mail.from
+    mail.deliver_now
+  end
+
   def test_event_invitee_message_with_multibyte_chars
     mail = EventMailer.event_invitee_message(event_invitee_messages(:one))
     assert_equal 'Så søte bær!', mail.subject
