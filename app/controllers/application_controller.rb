94238b2d6df66dd224ff0efb963e2933cb0b7989
donv <uwe@kubosch.no>
2019-04-10 17:07:54 +0200

    Start usiing "www.jujutsu.no" as the main web address.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   layout DEFAULT_LAYOUT
 
   if Rails.env.production?
-    before_action { redirect_to(host: 'jujutsu.no') if request.host != 'jujutsu.no' }
+    before_action { redirect_to(host: 'www.jujutsu.no') if request.host != 'www.jujutsu.no' }
   end
   before_action :reject_bots
   before_action :store_current_user_in_thread
