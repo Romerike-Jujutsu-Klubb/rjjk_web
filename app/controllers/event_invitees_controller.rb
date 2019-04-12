9d459fd6b89b3b42edced43f6b3d8b26800831ed
donv <uwe@kubosch.no>
2019-04-13 00:43:52 +0200

    Better user matching for old events

--- app/controllers/event_invitees_controller.rb
+++ app/controllers/event_invitees_controller.rb
@@ -54,8 +54,8 @@ class EventInviteesController < ApplicationController
     if @event_invitee.user
       @users.prepend @event_invitee.user
     else
-      @matching_email_user = User.find_by(email: @event_invitee.email)
-      @matching_phone_user = User.find_by(phone: @event_invitee.phone)
+      @matching_email_user = User.find_by(email: @event_invitee.email) if @event_invitee.email.present?
+      @matching_phone_user = User.find_by(phone: @event_invitee.phone) if @event_invitee.phone.present?
     end
   end
 end
