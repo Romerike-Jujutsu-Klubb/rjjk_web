ace1ea34b0ffb991d4073fea5bbdb1a177c04eb5
donv <uwe@kubosch.no>
2019-05-09 10:01:04 +0200

    Report differences after NKF member synchronization

--- app/jobs/nkf_member_sync_job.rb
+++ app/jobs/nkf_member_sync_job.rb
@@ -6,5 +6,22 @@ class NkfMemberSyncJob < ApplicationJob
   def perform(member)
     NkfMemberComparison.new(member).sync
     NkfMemberImport.new(member.nkf_member.medlemsnummer)
+    report_differences(member.reload)
+  end
+
+  private
+
+  def report_differences(member)
+    member.nkf_member.attributes.sort.each do |k, v|
+      target, mapped_key, mapped_value = NkfMember.rjjk_attribute(k, v)
+      next unless mapped_key
+      rjjk_value = @member.send(target)&.send(mapped_key)
+      v = v.strftime('%F %R') if v.is_a?(Time)
+      if mapped_value != rjjk_value
+        raise "Attribute did not sync: #{k}: #{v}: #{target}, #{mapped_key}, #{mapped_value}"
+      end
+    rescue => e
+      ExceptionNotifier.notify_exception(e)
+    end
   end
 end
