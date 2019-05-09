ec77cb6cca4347ceb59272e7247a9ae2ddf01566
donv <uwe@kubosch.no>
2019-05-09 10:38:37 +0200

    Allow <table> tags in popovers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,5 +49,12 @@ $(function () {
 });
 
 $(function () {
-  $('[data-toggle="popover"]').popover()
+    $.fn.popover.Constructor.Default.whiteList.table = [];
+    $.fn.popover.Constructor.Default.whiteList.tr = [];
+    $.fn.popover.Constructor.Default.whiteList.td = [];
+    $.fn.popover.Constructor.Default.whiteList.div = [];
+    $.fn.popover.Constructor.Default.whiteList.tbody = [];
+    $.fn.popover.Constructor.Default.whiteList.thead = [];
+
+    $('[data-toggle="popover"]').popover()
 });
