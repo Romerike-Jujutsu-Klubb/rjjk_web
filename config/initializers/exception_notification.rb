4fe36d10e5f9aa657508615803ac35901e45cf42
donv <uwe@kubosch.no>
2019-05-18 15:11:11 +0200

    Ignore failures by python bots

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -4,7 +4,7 @@ if Rails.env.production? || Rails.env.beta?
   env_prefix = ("#{Rails.env}." unless Rails.env.production?)
   Rails.application.config.middleware.use(
       ExceptionNotification::Rack,
-      ignore_crawlers: %w[Googlebot bingbot Mail.RU_Bot SemrushBot YandexBot],
+      ignore_crawlers: %w[Googlebot bingbot Mail.RU_Bot python-requests SemrushBot YandexBot],
       ignore_exceptions: [],
       email: {
         email_prefix: "[RJJK]#{"[#{Rails.env.upcase}]" unless Rails.env.production?} ",
