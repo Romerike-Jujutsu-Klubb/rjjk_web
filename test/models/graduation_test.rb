1592306b1fb01cef28305f454fe83217f05aa219
donv <uwe@kubosch.no>
2019-05-29 08:14:04 +0200

    Test that date cannot be changed after the graduation has been locked

--- test/models/graduation_test.rb
+++ test/models/graduation_test.rb
@@ -3,8 +3,12 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class GraduationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  test 'date cannot be changed after examiners/censors have confirmed' do
+    g = graduations(:panda)
+    g.censors.each { |c| c.locked_at = Time.current }
+    assert_raise ActiveRecord::RecordInvalid do
+      g.update! held_on: 2.days.from_now
+    end
+    assert_equal ['Dato kan ikke endres etter at graderingsoppsettet er låst'], g.errors.full_messages
   end
 end
