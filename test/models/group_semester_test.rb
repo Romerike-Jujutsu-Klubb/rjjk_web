7b738f5f2e6878a99d59e5a331abc049100c6ccf
donv <uwe@kubosch.no>
2019-08-07 00:18:28 +0200

    Make test predictable

--- test/models/group_semester_test.rb
+++ test/models/group_semester_test.rb
@@ -5,6 +5,6 @@ require 'test_helper'
 class GroupSemesterTest < ActiveSupport::TestCase
   test 'practices' do
     assert_equal([[], [], [327_850_042, 176_408_974], [], []],
-        GroupSemester.order(:id).to_a.map(&:practices).map(&:to_a).map { |a| a.map(&:id) })
+        GroupSemester.order(:id).to_a.map(&:practices).map(&:to_a).map { |a| a.sort_by(&:date).map(&:id) })
   end
 end
