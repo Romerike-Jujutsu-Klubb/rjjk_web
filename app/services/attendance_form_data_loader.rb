4d15f2ce5cf0859277b71e4663f070a666afe153
donv <uwe@kubosch.no>
2019-08-29 00:28:26 +0200

    Display instructors at the top of the attendance form

--- app/services/attendance_form_data_loader.rb
+++ app/services/attendance_form_data_loader.rb
@@ -10,9 +10,9 @@ module AttendanceFormDataLoader
     first_date = Date.new(@date.year, @date.month, 1)
     last_date = Date.new(@date.year, @date.month, -1)
 
+    @instructors = []
     if group_id
       if group_id == 'others'
-        @instructors = []
         @members = Member.includes(attendances: { group_schedule: :group })
             .where('id NOT in (SELECT DISTINCT member_id FROM group_memberships)')
             .where('left_on IS NULL OR left_on > ?', @date)
@@ -27,37 +27,50 @@ module AttendanceFormDataLoader
         weekdays = @group.group_schedules.map(&:weekday)
         @dates = (first_date..last_date).select { |d| weekdays.include? d.cwday }
 
-        @instructors = Member.active(@date)
+        if (chief_instructor = @group.current_semester&.chief_instructor)
+          @instructors << chief_instructor
+        end
+        group_instructors_query = GroupInstructor
+            .includes(:group_schedule,
+                member: [{ attendances: { practice: :group_schedule } }, :nkf_member])
+            .where(group_schedules: { group_id: @group.id })
+        if @instructors.any?
+          group_instructors_query = group_instructors_query
+              .where('group_instructors.member_id NOT IN (?)', @instructors.map(&:id))
+        end
+        @instructors += group_instructors_query.to_a.select { |gi| @dates.any? { |d| gi.active?(d) } }
+            .map(&:member).uniq
+        instructors_query = Member.active(@date)
             .includes({ attendances: { practice: :group_schedule },
                         graduates: %i[graduation rank] }, :groups, :nkf_member)
             .where(instructor: true)
-            .select { |m| m.groups.any? { |g| g.martial_art_id == @group.martial_art_id } }
-        @instructors.delete_if do |m|
-          m.attendances.select do |a|
-            ((@dates.first - 92.days)..@dates.last).cover?(a.date) &&
-                a.group_schedule.group_id == @group.id
-          end.empty?
+        if @instructors.any?
+          instructors_query = instructors_query.where('id NOT IN (?)', @instructors.map(&:id))
         end
-        @instructors += GroupInstructor
-            .includes(:group_schedule, member: [{ attendances: { practice: :group_schedule } },
-                                                :nkf_member])
-            .where('group_instructors.member_id NOT IN (?)', @instructors.map(&:id))
-            .where(group_schedules: { group_id: @group.id }).to_a
-            .select { |gi| @dates.any? { |d| gi.active?(d) } }.map(&:member).uniq
+        @instructors += instructors_query
+            .select { |m| m.groups.any? { |g| g.martial_art_id == @group.martial_art_id } }
+            .select do |m|
+              m.attendances.any? do |a|
+                ((@dates.first - 92.days)..@dates.last).cover?(a.date) &&
+                    a.group_schedule.group_id == @group.id
+              end
+            end
 
         current_members = @group.members.active(first_date, last_date)
             .includes({ attendances: { practice: :group_schedule },
                         graduates: %i[graduation rank], groups: :group_schedules },
                 :nkf_member)
-        attended_members = Member.references(:practices)
+        attended_query = Member.references(:practices)
             .includes(attendances: { practice: :group_schedule }, graduates: %i[graduation rank])
-            .where('members.id NOT IN (?)', @instructors.map(&:id))
             .where('practices.group_schedule_id IN (?)', @group.group_schedules.map(&:id))
             .where('year > ? OR ( year = ? AND week >= ?)',
                 first_date.cwyear, first_date.cwyear, first_date.cweek)
             .where('year < ? OR ( year = ? AND week <= ?)',
                 last_date.cwyear, last_date.cwyear, last_date.cweek)
-            .to_a
+        if @instructors.any?
+          attended_query = attended_query.where('members.id NOT IN (?)', @instructors.map(&:id))
+        end
+        attended_members = attended_query.to_a
         @members = current_members | attended_members
         @trials = @group.trials
 
@@ -71,7 +84,6 @@ module AttendanceFormDataLoader
         end
       end
     else
-      @instructors = []
       @members = []
       @trials = []
       current_members = []
