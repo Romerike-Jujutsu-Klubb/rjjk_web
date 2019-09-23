b74e4746a7d37d2f2cde1367d945b2b3bb49aaf1
donv <uwe@kubosch.no>
2019-09-23 23:19:45 +0200

    Mock import result

--- test/jobs/nkf_member_sync_job_test.rb
+++ test/jobs/nkf_member_sync_job_test.rb
@@ -6,8 +6,8 @@ class NkfMemberSyncJobTest < ActiveJob::TestCase
   include ActionMailer::TestHelper
 
   test 'sync' do
-    VCR.use_cassette('NKF Comparison', match_requests_on: %i[method host path query]) do
-      assert_emails(0) { NkfMemberSyncJob.perform_now(members(:lars)) }
+    VCR.use_cassette('NkfMemberSyncJob_lars', match_requests_on: %i[method host path query]) do
+      assert_emails(8) { NkfMemberSyncJob.perform_now(members(:lars)) }
     end
   end
 end
