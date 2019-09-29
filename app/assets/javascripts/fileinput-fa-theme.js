17e664e39bac93c965d62891c15717616fc06e2f
donv <uwe@kubosch.no>
2019-09-29 22:53:22 +0200

    Update icons to FA5

--- app/assets/javascripts/fileinput-fa-theme.js
+++ app/assets/javascripts/fileinput-fa-theme.js
@@ -19,10 +19,10 @@
             uploadIcon: '<i class="fa fa-upload text-info"></i>',
             zoomIcon: '<i class="fa fa-search-plus"></i>',
             dragIcon: '<i class="fa fa-bars"></i>',
-            indicatorNew: '<i class="fa fa-hand-o-down text-warning"></i>',
+            indicatorNew: '<i class="fa fa-hand-point-down text-warning"></i>',
             indicatorSuccess: '<i class="fa fa-check-circle text-success"></i>',
             indicatorError: '<i class="fa fa-exclamation-circle text-danger"></i>',
-            indicatorLoading: '<i class="fa fa-hand-o-up text-muted"></i>'
+            indicatorLoading: '<i class="fa fa-hand-point-up text-muted"></i>'
         },
         layoutTemplates: {
             fileIcon: '<i class="fa fa-file kv-caption-icon"></i> '
@@ -30,10 +30,10 @@
         previewZoomButtonIcons: {
             prev: '<i class="fa fa-caret-left fa-lg"></i>',
             next: '<i class="fa fa-caret-right fa-lg"></i>',
-            toggleheader: '<i class="fa fa-arrows-v"></i>',
+            toggleheader: '<i class="fa fa-arrows-alt-v"></i>',
             fullscreen: '<i class="fa fa-arrows-alt"></i>',
-            borderless: '<i class="fa fa-external-link"></i>',
-            close: '<i class="fa fa-remove"></i>'
+            borderless: '<i class="fa fa-external-link-alt"></i>',
+            close: '<i class="fa fa-times"></i>'
         },
         previewFileIcon: '<i class="fa fa-file"></i>',
         browseIcon: '<i class="fa fa-folder-open"></i>',
