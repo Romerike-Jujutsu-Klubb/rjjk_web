8f4d0c9fdeb65367f4e50166b32683bacea6139a
donv <uwe@kubosch.no>
2019-10-25 21:55:49 +0200

    Update gems including Rack::Attack with new config Remove coffee-rails since it is not used

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-class Rack::Attack
-  throttle('req/ip', limit: 300, period: 5.minutes, &:ip)
-  throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
-    req.ip if req.path =~ %r{/login} && req.post?
+if Rails.env.test?
+  Rack::Attack.enabled = false
+else
+  class Rack::Attack
+    throttle('req/ip', limit: 300, period: 5.minutes, &:ip)
+    throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
+      req.ip if req.path =~ %r{/login} && req.post?
+    end
+    throttle('logins/email', limit: 5, period: 20.seconds) do |req|
+      req.params['email'].presence if req.path =~ %r{/login} && req.post?
+    end
+    throttle('logins/key', limit: 5, period: 20.seconds) { |req| req.params['key'].presence && :key }
   end
-  throttle('logins/email', limit: 5, period: 20.seconds) do |req|
-    req.params['email'].presence if req.path =~ %r{/login} && req.post?
-  end
-  throttle('logins/key', limit: 5, period: 20.seconds) { |req| req.params['key'].presence && :key }
 end
