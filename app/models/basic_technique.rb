3afa3f1325b0e48086dbbf6e0ff13c3d3a891947
donv <uwe@kubosch.no>
2019-11-30 18:59:47 +0100

    Bedre redigering av linker til grunnteknikker

--- app/models/basic_technique.rb
+++ app/models/basic_technique.rb
@@ -5,6 +5,8 @@ class BasicTechnique < ApplicationRecord
   belongs_to :waza
   has_many :basic_technique_links, dependent: :destroy
 
+  before_validation { description.strip! }
+
   validates :name, :waza_id, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 end
