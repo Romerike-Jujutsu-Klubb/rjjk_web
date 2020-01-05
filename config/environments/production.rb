0b5d714a8da156e1d67041a0e3afb8547b843caf
donv <uwe@kubosch.no>
2020-01-05 16:58:49 +0100

    Remove obsolete config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,9 +11,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { protocol: 'https', host: 'www.jujutsu.no' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    # user_name: ENV['SENDGRID_USERNAME'],
-    # password: ENV['SENDGRID_PASSWORD'],
-    # address: 'smtp.sendgrid.net',
     user_name: ENV['SMTP_USERNAME'].gsub('@', "-#{Date.current.cwday}@"),
     password: ENV['SMTP_PASSWORD'],
     address: 'smtp.webhuset.no',
