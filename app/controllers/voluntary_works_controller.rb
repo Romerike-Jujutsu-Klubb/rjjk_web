39ddf376134da0435b7ed8260ef40267105e43fe
donv <uwe@kubosch.no>
2020-01-08 14:13:30 +0100

    Enable adding participants to Event subclass VoluntaryWork

--- app/controllers/voluntary_works_controller.rb
+++ app/controllers/voluntary_works_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class VoluntaryWorksController < EventsController
+  before_action {params[:event] ||= params[:voluntary_work]}
   # def self.local_prefixes
   #  super + ['events']
   # end
