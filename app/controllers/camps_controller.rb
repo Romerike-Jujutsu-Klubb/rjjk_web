ab28c7cea5ed24facb7bb81dbd99bba66da90f1b
donv <uwe@kubosch.no>
2020-01-15 23:32:27 +0100

    Behandle endringer for leir

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class CampsController < EventsController
+  before_action { params[:event] ||= params[:camp] }
   # def self.local_prefixes
   #  super + ['events']
   # end
