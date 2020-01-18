8da003befdebc5b12b038420e046eb881b235114
donv <uwe@kubosch.no>
2020-01-18 14:22:14 +0100

    Handle Event subclasses in the EventController, for simplicity.

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -3,6 +3,11 @@
 class Camp < Event
   HEADER = 'RJJK Leir'
 
+  # TODO(uwe): Consider https://gist.github.com/sj26/5843855
+  def self.model_name
+    superclass.model_name
+  end
+
   def title
     name.present? ? "#{HEADER}: #{name}" : HEADER
   end
