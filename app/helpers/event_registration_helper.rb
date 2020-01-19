9350f7f12b7b1ccd6a7d83b154fc8d1b4f46d7b9
donv <uwe@kubosch.no>
2020-01-19 17:20:03 +0100

    Link to external event registration link

--- app/helpers/event_registration_helper.rb
+++ app/helpers/event_registration_helper.rb
@@ -4,4 +4,8 @@ module EventRegistrationHelper
   def event_registration_link(event)
     event.registration_url || new_event_registration_path(event_invitee: { event_id: event.id })
   end
+
+  def event_registration_target(event)
+    "event_#{event.id}" if event.registration_url
+  end
 end
