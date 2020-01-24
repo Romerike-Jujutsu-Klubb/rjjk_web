9757898735f1e87a067812851df3107f509ba252
donv <uwe@kubosch.no>
2020-01-24 10:28:04 +0100

    Improve test coverage

--- test/models/event_invitee_message_test.rb
+++ test/models/event_invitee_message_test.rb
@@ -3,12 +3,29 @@
 require 'test_helper'
 
 class EventInviteeMessageTest < ActiveSupport::TestCase
-  test 'initiaslize SIGNUP_CONFIRMATION' do
+  test 'initialize INVITATION' do
+    EventInviteeMessage.new(message_type: EventMessage::MessageType::INVITATION,
+    event_invitee: event_invitees(:one))
+  end
+
+  test 'initialize INVITATION en' do
+    users(:newbie).update! locale: 'en'
+    EventInviteeMessage.new(message_type: EventMessage::MessageType::INVITATION,
+    event_invitee: event_invitees(:one))
+  end
+
+  test 'initialize SIGNUP_CONFIRMATION' do
     EventInviteeMessage.new(message_type: EventInviteeMessage::MessageType::SIGNUP_CONFIRMATION,
     event_invitee: event_invitees(:one))
   end
-  test 'initiaslize SIGNUP_REJECTION' do
+
+  test 'initialize SIGNUP_REJECTION' do
     EventInviteeMessage.new(message_type: EventInviteeMessage::MessageType::SIGNUP_REJECTION,
     event_invitee: event_invitees(:one))
+  end
+
+  test 'existing event message' do
+    EventInviteeMessage.new(message_type: EventMessage::MessageType::REMINDER,
+        event_invitee: event_invitees(:one))
   end
 end
