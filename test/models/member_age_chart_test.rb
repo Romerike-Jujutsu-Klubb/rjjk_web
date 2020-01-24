8e9ea0d795244884892caa82bb5f4fb03f2873e4
donv <uwe@kubosch.no>
2020-01-24 09:53:27 +0100

    Clear cache to get better coverage

--- test/models/member_age_chart_test.rb
+++ test/models/member_age_chart_test.rb
@@ -4,6 +4,25 @@ require 'test_helper'
 
 class MemberAgeChartTest < ActiveSupport::TestCase
   def test_chart
-    MemberAgeChart.data_set
+    Rails.cache.delete('member_age_chart_data/2013-10-17_16:46:00.000000000')
+    assert_equal 2, MemberAgeChart.data_set.size
+    assert_equal [
+      {
+        name: 'Totalt',
+        data: [['0..5', 0], ['6..9', 0], ['10..14', 0], ['15..19', 0], ['20..24', 1], ['25..29', 0],
+               ['30..39', 0], ['40..49', 3], ['50..50', 0]],
+        color: :black,
+      }, {
+        name: 'Betalende',
+        data: [['0..5', 0], ['6..9', 0], ['10..14', 0], ['15..19', 0], ['20..24', 0], ['25..29', 0],
+               ['30..39', 0], ['40..49', 0], ['50..50', 0]],
+        color: :red,
+      }, {
+        name: 'Aktive',
+        data: [['0..5', 0], ['6..9', 0], ['10..14', 0], ['15..19', 0], ['20..24', 1], ['25..29', 0],
+               ['30..39', 0], ['40..49', 3], ['50..50', 0]],
+        color: :green,
+      }
+    ], MemberAgeChart.data_set[1]
   end
 end
