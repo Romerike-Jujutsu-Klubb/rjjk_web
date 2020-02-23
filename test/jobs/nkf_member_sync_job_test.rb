fc7d51cfacca210c1c9633fc4cd670155cbe0543
donv <uwe@kubosch.no>
2020-02-23 22:26:14 +0100

    Clear mail queue before test

--- test/jobs/nkf_member_sync_job_test.rb
+++ test/jobs/nkf_member_sync_job_test.rb
@@ -2,9 +2,7 @@
 
 require 'test_helper'
 
-class NkfMemberSyncJobTest < ActiveJob::TestCase
-  include ActionMailer::TestHelper
-
+class NkfMemberSyncJobTest < ActionMailer::TestCase
   test 'sync' do
     VCR.use_cassette('NkfMemberSyncJob_lars', match_requests_on: %i[method host path query]) do
       assert_emails(12) { NkfMemberSyncJob.perform_now(members(:lars)) }
