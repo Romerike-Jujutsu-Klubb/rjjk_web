5f841d757b5ec78e5a140a5b45c1bcdaca0f347e
donv <uwe@kubosch.no>
2020-04-06 21:06:29 +0200

    Use SendGrid to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { protocol: 'https', host: 'www.jujutsu.no' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: ENV['SMTP_USERNAME'].sub('@', "#{Date.current.cwday}@"),
+    user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     address: ENV['SMTP_SERVER'],
     domain: 'jujutsu.no',
