e83c89162f97edb4d0cca423d02b5165ff269a83
donv <uwe@kubosch.no>
2020-04-27 18:42:57 +0200

    Allow user valid ajax queries

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
-if Rails.env.development? || Rails.env.test?
+USER_VALID_PATTERN = %r{/users/\d+/valid }.freeze
+
+if Rails.env.test?
   Rack::Attack.enabled = false
 else
   class Rack::Attack
+    safelist('PWA') do |request|
+      ['/manifest.json', '/offline.html', '/serviceworker.js'].include?(request.path) ||
+          (USER_VALID_PATTERN.match?(request.path) && request.env['rack.session']['user_id'])
+    end
+
     throttle('req/ip', limit: 300, period: 5.minutes, &:ip)
     throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
       req.ip if req.path =~ %r{/login} && req.post?
