558b5d7bc1d93ccd4dc814e439442f6a37521043
donv <uwe@kubosch.no>
2020-05-18 01:02:03 +0200

    Vis lenker i redigering av applikasjon

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../new_front_page
+//= link cloudinary.js
 //= link new_front_page.css
 //= link_directory ../stylesheets .css
