1192e8cfbb5a5cdf98f937a4c6e181a6b44973cb
donv <uwe@kubosch.no>
2020-06-11 10:40:51 +0200

    Always display practice for attendance

--- app/controllers/attendance_form_controller.rb
+++ app/controllers/attendance_form_controller.rb
@@ -10,10 +10,15 @@ class AttendanceFormController < ApplicationController
     @my_groups = current_user.member.instruction_groups
     if params[:practice_id]
       @practice = Practice.find(params[:practice_id])
-    elsif (last_practice = @my_groups.map(&:last_practice).max_by(&:date)).date == Date.current
-      @practice = last_practice
-    elsif (next_practice = @my_groups.map(&:next_practice).min_by(&:date)).date == Date.current
-      @practice = next_practice
+    else
+      next_practice = @my_groups.map(&:next_practice).min_by(&:date)
+      if next_practice.date == Date.current
+        @practice = next_practice
+      elsif (last_practice = @my_groups.map(&:last_practice).max_by(&:date)).date >= Date.yesterday
+        @practice = last_practice
+      else
+        @practice = next_practice
+      end
     end
   end
 
