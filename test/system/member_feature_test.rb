1152403fc9e1272f52e466626d2cb4b50685320c
donv <uwe@kubosch.no>
2020-06-17 19:17:31 +0200

    Test user form

--- test/system/member_feature_test.rb
+++ test/system/member_feature_test.rb
@@ -3,14 +3,11 @@
 require 'application_system_test_case'
 
 class MemberFeatureTest < ApplicationSystemTestCase
-  setup { screenshot_section :member }
-
   def test_show
-    screenshot_group :show
     visit_with_login member_path(members(:sebastian)), redirected_path: '/users/594055058'
-    screenshot :person
-    find('a.nav-link', text: 'Medlemskap').click
     screenshot :membership
+    find('a.nav-link', text: 'Personalia').click
+    screenshot :person
     find('a.nav-link', text: 'Graderinger').click
     screenshot :graduations
     find('a.nav-link', text: 'Verv').click
