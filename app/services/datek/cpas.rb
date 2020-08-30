a7c179c6a553a49d112a8b4d7975c3bae81b76bc
Uwe Kubosch <uwe@kubosch.no>
2020-08-30 13:36:45 +0200

    Allow Rails credential for CPAS auth

--- app/services/datek/cpas.rb
+++ app/services/datek/cpas.rb
@@ -5,18 +5,22 @@ require 'cgi'
 
 module Datek
   module Cpas
-    BASE_URL = ENV['CPAS_API_BASE_URL'] ||
-        (defined?(Rails) &&
-            (Rails.application&.credentials&.cpas_api_base_url ||
-                ((Rails.env.development? || Rails.env.test?) && 'https://example.com/sms'))) ||
-        raise('CPAS_API_BASE_URL not set')
+    def self.base_url
+      ENV['CPAS_API_BASE_URL'] ||
+          (defined?(Rails) &&
+              (Rails.application&.credentials&.cpas_api_base_url ||
+                  ((Rails.env.development? || Rails.env.test?) && 'https://example.com/sms'))) ||
+          raise('CPAS_API_BASE_URL not set')
+    end
 
     def self.send_sms(to:, text:)
       ctx = OpenSSL::SSL::SSLContext.new
       ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      HTTP.headers('content_type' => 'application/x-www-form-urlencoded')
-          .post("#{BASE_URL}/send/mt", ssl_context: ctx,
-              body: "to=#{CGI.escape to.to_s}&text=#{CGI.escape text}")
+      request = HTTP.headers('content_type' => 'application/x-www-form-urlencoded')
+      uri = URI(base_url)
+      request = request.basic_auth(user: uri.user, pass: uri.password) if uri.userinfo
+      request.post("#{uri}/send/mt", ssl_context: ctx,
+          body: "to=#{CGI.escape to.to_s}&text=#{CGI.escape text}")
     end
   end
 end
