2e0a8a0f7d0da7d6897a73c484b48a6063329358
Uwe Kubosch <uwe@kubosch.no>
2020-09-11 07:26:27 +0200

    Better member drilldown

--- app/controllers/member_drilldown_controller.rb
+++ app/controllers/member_drilldown_controller.rb
@@ -3,37 +3,24 @@
 class MemberDrilldownController < SimpleDrilldown::Controller
   before_action { @content_width = 1024 }
 
-  # What fields should be displayed as default when listing actual Member records.
-  default_fields %w[name created_at updated_at] # TODO(uwe): Read fields from schema?
-
-  # When listing records, what relations should be included for optimization?
-  # list_includes :user, :comments # TODO(uwe): Read relations from schema?
-
-  # In what order should records be listed?
-  list_order 'members.created_at'
-
-  # Field definitions when listing records
   field :name
-  field :created_at
-  field :updated_at
+  field :joined_on
+  default_fields %w[name joined_on]
+  list_order 'members.joined_on, members.created_at'
 
-  # The "attr_method" option transforms the value from the database to a
-  # readable form.
-  # field :user, attr_method: ->(post) { post.user.name }
-  # field :body, attr_method: ->(post) { post.body[0..32] }
-  # field :comments, attr_method: ->(post) { post.comments.count }
-
-  dimension :calendar_date, 'DATE(members.created_at)', interval: true
-  dimension :day_of_month, "date_part('day', members.created_at)::int"
-  dimension :day_of_week, <<~SQL, label_method: ->(day_no) { Date::DAYNAMES[day_no.to_i % 7] }
-    CASE WHEN date_part('dow', members.created_at) = 0 THEN 7 ELSE date_part('dow', members.created_at)::int END
+  dimension :active, <<~SQL
+    CASE
+    WHEN left_on IS NOT NULL AND left_on < CURRENT_DATE THEN 'Utmeldt'
+    WHEN honorary_on IS NOT NULL AND honorary_on < CURRENT_DATE THEN 'Æresmedlem'
+    WHEN passive_on IS NOT NULL AND passive_on < CURRENT_DATE THEN 'Passiv'
+    ELSE 'Aktiv'
+    END
   SQL
-  dimension :hour_of_day, "date_part('hour', members.created_at)::int"
-  dimension :month, "date_part('month', members.created_at)::int",
+  dimension :calendar_date, 'DATE(members.joined_on)', interval: true
+  dimension :instructor, 'EXISTS(SELECT id FROM group_instructors WHERE member_id = members.id)',
+      label_method: ->(bool) { bool ? 'Ja' : 'Nei' }
+  dimension :month, "date_part('month', members.joined_on)::int",
       label_method: ->(month_no) { Date::MONTHNAMES[month_no.to_i] }
-  dimension :week, "date_part('week', members.created_at)::int"
-  dimension :year, "date_part('year', members.created_at)::varchar"
-
-  # dimension :comments, 'SELECT count(*) FROM comments c WHERE c.member_id = members.id'
-  # dimension :user, 'users.name', includes: :user
+  dimension :week, "date_part('week', members.joined_on)::int"
+  dimension :year, "date_part('year', members.joined_on)::varchar"
 end
