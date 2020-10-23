54d542b4e65dd82102c0343e52f3eebacb4be8ba
donv <uwe@kubosch.no>
2020-10-23 18:59:06 +0200

    Add bootstrap JS for modals

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -2,3 +2,5 @@
 //= require @fortawesome/fontawesome-free/js/all.js
 //= require @fortawesome/fontawesome-free/js/v4-shims
 //= require jquery/dist/jquery
+//= require bootstrap/js/dist/util
+//= require bootstrap/js/dist/modal
