1916e9924a021cff15169ed6788aec65caa93957
donv <uwe@kubosch.no>
2020-11-03 11:52:19 +0100

    Make migration reversible

--- db/migrate/20201103102820_add_reg_dato_to_nkf_member_trials.rb
+++ db/migrate/20201103102820_add_reg_dato_to_nkf_member_trials.rb
@@ -3,6 +3,6 @@
 class AddRegDatoToNkfMemberTrials < ActiveRecord::Migration[6.0]
   def change
     add_column :nkf_member_trials, :reg_dato, :date, null: false, default: Date.current
-    change_column_default :nkf_member_trials, :reg_dato, nil
+    change_column_default :nkf_member_trials, :reg_dato, from: Date.current, to: nil
   end
 end
