50474f1970a3448baa8cbbad797e06e65f42a137
Uwe Kubosch <uwe@kubosch.no>
2020-11-05 17:54:36 +0100

    Only display synchronized NKF fields

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-HOSTED = %w[production beta].include?(get(:environment))
-max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { HOSTED ? 5 : 1 }
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 6 }
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 port ENV.fetch('PORT', 3000)
@@ -9,7 +8,7 @@ environment ENV.fetch('RAILS_ENV', 'development')
 pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 worker_count = ENV.fetch('WEB_CONCURRENCY', 2).to_i
 if worker_count > 1
-  workers worker_count if HOSTED
+  workers worker_count
   preload_app!
   on_worker_boot { ActiveRecord::Base.establish_connection }
 else
