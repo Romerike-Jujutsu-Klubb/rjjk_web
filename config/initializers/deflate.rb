c70bc9fe8feec3ac70665d48b014dffa6ada49e3
Uwe Kubosch <uwe@kubosch.no>
2020-11-12 18:30:46 +0100

    Try to figure out webpack setup

--- config/initializers/deflate.rb
+++ config/initializers/deflate.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-Rails.application.config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
+if Rails.application.config.public_file_server.enabled
+  Rails.application.config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
+end
