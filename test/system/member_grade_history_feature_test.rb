59f87634b5c7d02d1b37b982c9f3f9a9e93f4e33
Uwe Kubosch <uwe@kubosch.no>
2020-12-25 18:16:51 +0100

    Ensure chart is loaded

--- test/system/member_grade_history_feature_test.rb
+++ test/system/member_grade_history_feature_test.rb
@@ -5,6 +5,8 @@ require 'application_system_test_case'
 class MemberGradeHistoryFeatureTest < ApplicationSystemTestCase
   def test_grade_history
     visit_with_login member_reports_grade_history_graph_path
+    assert_css "#chart-1 canvas"
+    assert_css "#chart-2 canvas"
     screenshot('grade_history_graph', color_distance_limit: 79.1)
   end
 end
